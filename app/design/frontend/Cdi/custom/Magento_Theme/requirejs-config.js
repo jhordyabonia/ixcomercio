aa10e6a06a550b4ddea92630dbc1ce70e9f4b703
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-04-28 18:24:00 -0500

    fix color v.1.0.7

--- app/design/frontend/Cdi/custom/Magento_Theme/requirejs-config.js
+++ app/design/frontend/Cdi/custom/Magento_Theme/requirejs-config.js
@@ -1,16 +1,14 @@
 var config = {
     paths: {
-    	"*": {
-	    	"bootstrap": "Magento_Theme/js/bootstrap.bundle",
-	      	"mainJs": "Magento_Theme/js/main.min"
-	    }
+    	"bootstrap": "Magento_Theme/js/bootstrap.bundle",
+      	"mainJs": "Magento_Theme/js/main.min"
     },
     shim: {
-        "bootstrap": {
-            "deps": ["jquery"]
+        'bootstrap': {
+            'deps': ['jquery']
         },
-        "mainJs": {
-        	"deps": ['jquery']  
+        'mainJs': {
+        	'deps': ['jquery']
         }
     }
 };
