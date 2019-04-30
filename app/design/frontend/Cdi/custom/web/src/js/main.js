40c59e46c20d6beeda19064a789cb5b4a936ca6f
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-04-30 06:22:35 -0500

    menu mobile v.1.0.2

--- app/design/frontend/Cdi/custom/web/src/js/main.js
+++ app/design/frontend/Cdi/custom/web/src/js/main.js
@@ -1,33 +1,28 @@
-require(['jquery', 'mainJs', 'domReady!'], function($) {
-    $(window).load(function() {
 
-		jQuery(document).ready(function(){
+jQuery(document).ready(function(){
 
-			if(jQuery(".product-options-wrapper .swatch-attribute.jam_color").length){
-				jQuery(".product-options-wrapper .swatch-attribute.jam_color .swatch-attribute-options .swatch-option").hover(function(){
-					var labelColor = jQuery(this).attr("option-label");
-					jQuery(".swatch-attribute-selected-option").text(labelColor);
-				}, function(){
-					jQuery(".swatch-attribute-selected-option").text(jQuery(".product-options-wrapper .swatch-attribute.jam_color .swatch-attribute-options .swatch-option.selected").attr("option-label"));
-				})
-			}
+	if(jQuery(".product-options-wrapper .swatch-attribute.jam_color").length){
+		jQuery(".product-options-wrapper .swatch-attribute.jam_color .swatch-attribute-options .swatch-option").hover(function(){
+			var labelColor = jQuery(this).attr("option-label");
+			jQuery(".swatch-attribute-selected-option").text(labelColor);
+		}, function(){
+			jQuery(".swatch-attribute-selected-option").text(jQuery(".product-options-wrapper .swatch-attribute.jam_color .swatch-attribute-options .swatch-option.selected").attr("option-label"));
+		})
+	}
 
-		});
+});
 
-		var windowScroll_t;
-		jQuery(window).scroll(function(){
-			clearTimeout(windowScroll_t);
-			windowScroll_t = setTimeout(function() {
-				if (jQuery(this).scrollTop() > 100){
-					jQuery('#scroll-to-top').addClass("show");
-				}else{
-					jQuery('#scroll-to-top').removeClass("show");
-				}
-			}, 500);
-		});
-		jQuery('#scroll-to-top').click(function(){
-			jQuery( "html, body" ).animate({scrollTop:0}, 500, 'swing');
-		});
-
-	});
+var windowScroll_t;
+jQuery(window).scroll(function(){
+	clearTimeout(windowScroll_t);
+	windowScroll_t = setTimeout(function() {
+		if (jQuery(this).scrollTop() > 100){
+			jQuery('#scroll-to-top').addClass("show");
+		}else{
+			jQuery('#scroll-to-top').removeClass("show");
+		}
+	}, 500);
+});
+jQuery('#scroll-to-top').click(function(){
+	jQuery( "html, body" ).animate({scrollTop:0}, 500, 'swing');
 });
