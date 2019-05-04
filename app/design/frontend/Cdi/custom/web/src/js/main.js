68b53b0d676c749d3244697c26fb0e103e659f61
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-05-04 16:17:29 -0500

    scroll top

--- app/design/frontend/Cdi/custom/web/src/js/main.js
+++ app/design/frontend/Cdi/custom/web/src/js/main.js
@@ -14,17 +14,18 @@ require(['jquery', 'mainJs', 'domReady!'], function($) {
 
 		});
 
-		jQuery(window).on("scroll", function(){
-			if(jQuery(window).scrollTop() > 300){
-				jQuery("#scroll-to-top").addClass("show");
-			}else{
-				jQuery("#scroll-to-top").removeClass("show");
-			}
-			
-		})
 		jQuery('#scroll-to-top').click(function(){
 			jQuery( "html, body" ).animate({scrollTop:0}, 500, 'swing');
 		});
 
+	});
+
+	jQuery(window).on("scroll", function(){
+		if(jQuery(window).scrollTop() > 300){
+			jQuery("#scroll-to-top").addClass("show");
+		}else{
+			jQuery("#scroll-to-top").removeClass("show");
+		}
+		
 	});
 });
