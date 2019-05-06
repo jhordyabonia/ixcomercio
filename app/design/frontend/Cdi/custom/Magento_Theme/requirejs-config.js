660dbcb63eafcfc6b6cf96d6042b6ddce273cc47
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-05-05 21:06:05 -0500

    footer

--- app/design/frontend/Cdi/custom/Magento_Theme/requirejs-config.js
+++ app/design/frontend/Cdi/custom/Magento_Theme/requirejs-config.js
@@ -1,12 +1,14 @@
 var config = {
 	map: {
-       '*': {
-           mainJs: 'js/main.min',
+      '*': {
+        mainJs: 'js/main.min',
+        zoomJs: 'lib/zoom/jquery.zoom.min'
 		}
 	},
     paths: {
     	"bootstrap": "Magento_Theme/js/bootstrap.bundle",
-      	"mainJs": "Magento_Theme/js/main.min"
+      "mainJs": "Magento_Theme/js/main.min",
+      "zoomJs": "Magento_Theme/lib/zoom/jquery.zoom.min"
     },
     shim: {
         'bootstrap': {
@@ -14,6 +16,9 @@ var config = {
         },
         'mainJs': {
         	'deps': ['jquery']
+        },
+        'zoomJs': {
+          'deps': ['jquery']
         }
     }
 };
--- app/design/frontend/Cdi/custom/web/src/js/main.js
+++ app/design/frontend/Cdi/custom/web/src/js/main.js
@@ -1,4 +1,4 @@
-require(['jquery', 'mainJs', 'domReady!'], function($) {
+require(['jquery', 'mainJs', 'zoomJs', 'domReady!'], function($) {
     
 	jQuery('#scroll-to-top').click(function(){
 		jQuery("html, body").animate({scrollTop: 0}, 600, "easeOutCubic");
