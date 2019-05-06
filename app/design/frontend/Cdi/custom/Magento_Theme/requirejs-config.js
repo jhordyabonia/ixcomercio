eaaf029ffed47a75efe52dfeda2ee4891f99ce25
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-05-05 21:09:12 -0500

    footer

--- app/design/frontend/Cdi/custom/Magento_Theme/requirejs-config.js
+++ app/design/frontend/Cdi/custom/Magento_Theme/requirejs-config.js
@@ -2,13 +2,13 @@ var config = {
 	map: {
       '*': {
         mainJs: 'js/main.min',
-        zoomJs: 'lib/zoom/jquery.zoom.min'
+        zoomJs: 'js/jquery.zoom.min'
 		}
 	},
     paths: {
     	"bootstrap": "Magento_Theme/js/bootstrap.bundle",
       "mainJs": "Magento_Theme/js/main.min",
-      "zoomJs": "Magento_Theme/lib/zoom/jquery.zoom.min"
+      "zoomJs": "Magento_Theme/js/jquery.zoom.min"
     },
     shim: {
         'bootstrap': {
