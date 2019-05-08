0899b079729b1eeb62359237dc0c0dac8dfc4182
Jhonatan holguín <jholguin9004@gmail.com>
2019-05-08 00:55:20 -0500

    agrega js para product registration

---
+++ app/design/frontend/Cdi/custom/web/js/cms/product_registration1.js
@@ -0,0 +1,1 @@
+var host = (("https:" == document.location.protocol) ? "https://secure." : "http://");document.write(unescape("%3Cscript src='" + host + "wufoo.com/scripts/embed/form.js' type='text/javascript'%3E%3C/script%3E"));
---
+++ app/design/frontend/Cdi/custom/web/js/cms/product_registration2.js
@@ -0,0 +1,8 @@
+var w7x3s5 = new WufooForm();
+w7x3s5.initialize({
+'userName':'powermat', 
+'formHash':'w7x3s5', 
+'autoResize':true,
+'height':'1660', 
+'ssl':true});
+w7x3s5.display();
