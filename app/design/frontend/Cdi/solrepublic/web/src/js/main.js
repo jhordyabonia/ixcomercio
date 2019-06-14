4327c8590be3bffb62c72849338b8cab61d862d5
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-06-14 04:59:34 -0500

    header animate

--- app/design/frontend/Cdi/solrepublic/web/src/js/main.js
+++ app/design/frontend/Cdi/solrepublic/web/src/js/main.js
@@ -16,6 +16,47 @@ require(['jquery', 'zoomJs', 'mainJs', 'domReady!'], function($) {
 	});
 
 
+	jQuery(document).ready(function(){
+	// Hide Header on on scroll down
+	    var didScroll;
+	    var lastScrollTop = 0;
+	    var delta = 5;
+	    var navbarHeight = jQuery('.page-header').outerHeight();
+
+	    jQuery(window).scroll(function(event){
+	        didScroll = true;
+	    });
+
+	    setInterval(function() {
+	        if (didScroll) {
+	            hasScrolled();
+	            didScroll = false;
+	        }
+	    }, 250);
+
+	    function hasScrolled() {
+	        var st = jQuery(this).scrollTop();
+
+	        // Make sure they scroll more than delta
+	        if(Math.abs(lastScrollTop - st) <= delta)
+	            return;
+
+	        // If they scrolled down and are past the navbar, add class .nav-up.
+	        // This is necessary so you never see what is "behind" the navbar.
+	        if (st > lastScrollTop && st > navbarHeight){
+	            // Scroll Down
+	            jQuery('.page-header').removeClass('nav-down').addClass('nav-up');
+	        } else {
+	            // Scroll Up
+	            if(st + jQuery(window).height() < jQuery(document).height()) {
+	                jQuery('.page-header').removeClass('nav-up').addClass('nav-down');
+	            }
+	        }
+	        lastScrollTop = st;
+	    }
+	});
+
+
 	jQuery(window).on("scroll", function(){
 		if(jQuery(window).scrollTop() > 300){
 			jQuery("#scroll-to-top").addClass("show");
