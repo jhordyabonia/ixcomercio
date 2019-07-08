5b85df2522126805f46c8d932531676a2cf0e036
Jhonatan holguín <jholguin9004@gmail.com>
2019-07-07 19:44:29 -0500

    agrega attributes para marley

--- app/code/Cdi/Custom/Model/Images.php
+++ app/code/Cdi/Custom/Model/Images.php
@@ -15,19 +15,29 @@ class Images
     public static function getAvailableImages()
     {
         return [
-            'water.png' => __('Water'),
-            'time.png' => __('Time'),
-            'location.png' => __('Range'),
-            'cable.png' => __('Cable'),
-            'handsfree.png' => __('Hands Free'),
-            'pairing.png' => __('Pairing'),
-            'speakerphone.png' => __('Speaker Phone'),
-            'anc.png' => __('ANC'),
-            'convenientControls.png' => __('Convenient controls'),
-            'CordManagement.png' => __('Cord Management'),
-            'OnEarDesign.png' => __('On Ear Design'),
-            'StickyPad.png' => __('Sticky Pad'),
-            'TrulyWireless.png' => __('Truly Wireless'),
+			'Jam' => [
+				'water.png' => __('Water'),
+				'time.png' => __('Time'),
+				'location.png' => __('Range'),
+				'cable.png' => __('Cable'),
+				'handsfree.png' => __('Hands Free'),
+				'pairing.png' => __('Pairing'),
+				'speakerphone.png' => __('Speaker Phone'),
+				'anc.png' => __('ANC'),
+				'convenientControls.png' => __('Convenient controls'),
+				'CordManagement.png' => __('Cord Management'),
+				'OnEarDesign.png' => __('On Ear Design'),
+				'StickyPad.png' => __('Sticky Pad'),
+				'TrulyWireless.png' => __('Truly Wireless'),			
+			],
+			'Marley' => [
+				'aluminium.PNG' => 'Aluminium',
+				'organic_cotton.PNG' => 'Organic cotton',
+				'recycead_plastic.PNG' => 'Recycead plastic',
+				'redwind.PNG' => 'Redwind fabric',
+				'regrind.PNG' => 'Regrind',
+				'wood_fiber.PNG' => 'Wood fiber',
+			]
         ];
     }
 }
