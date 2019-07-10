2e145da0e5f2ff1f91f0878aedc2ed03b0acd125
root <christian.quimbay@ariadnacg.com>
2019-07-10 16:19:36 +0000

    Se agregan archivos desde composer

---
+++ app/code/Trax/Catalogo/Setup/UpgradeData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Trax\Catalogo\Setup;
+
+use Magento\Eav\Setup\EavSetup; 
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory) {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context){
+        $setup->startSetup();
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);		
+		if(version_compare($context->getVersion(), '1.0.2', '<')){
+            $eavSetup->addAttribute(\Magento\Catalog\Model\Category::ENTITY, 'iws_id', [
+                'type'     => 'varchar',
+                'label'    => 'Id IWS',
+                'input'    => 'text',
+                'source'   => '',
+                'visible'  => true,
+                'required' => false,
+                'default'  => '',
+                'global'   => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'group'    => 'Display Settings',
+            ]);			
+		}
+        $setup->endSetup();
+    }
+}
