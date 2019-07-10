10e83852168bc34c117a1c433d27663c8a194277
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-10 15:04:30 -0500

    Se corrige método en openpay 9

--- app/code/Openpay/Cards/Model/Payment.php
+++ app/code/Openpay/Cards/Model/Payment.php
@@ -389,7 +389,7 @@ class Payment extends \Magento\Payment\Model\Method\Cc
             $charge = $this->makeOpenpayCharge($customer_data, $charge_request, $token, $device_session_id, $save_cc, $openpay_cc);   
             $this->logger->debug('#processCapture', array('charge_id' => $charge->id)); 
             
-        $this->logger->debug('#processCapture', array('datos' => json_encode($charge))); 
+        $this->logger->debug('#processCapture', array('datos' => $charge)); 
             $payment->setTransactionId($charge->id);  
             $payment->setCcLast4(substr($charge->card_number, -4));
             $payment->setCcType($this->getCCBrandCode($charge->brand));
