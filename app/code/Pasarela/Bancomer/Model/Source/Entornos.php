86b9a19c1b6ea7bdca78769081d35dec8ce89a27
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-22 15:19:41 -0500

    whitelabel - Se cambian parametros de configuracion 1

---
+++ app/code/Pasarela/Bancomer/Model/Source/Entornos.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Trax\Catalogo\Model\Config\Source;
+
+class Entornos implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 0, 'label' => __('Sandbox')],
+            ['value' => 1, 'label' => __('Produccion')],
+        ];
+    }
+}
