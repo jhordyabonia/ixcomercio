c5b19431455e4c94266b8f1b81f08c0de9bd8ef3
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-23 10:54:31 -0500

    Whitelabel - Se valida redirección a multipagos

---
+++ app/code/Pasarela/Bancomer/Controller/Payment/Success.php
@@ -0,0 +1,81 @@
+<?php
+/** 
+ * @category    Payments
+ * @package     Pasarela_Bancomer
+ * @author      Valentina Aguirre
+ * @license     http://www.apache.org/licenses/LICENSE-2.0  Apache License Version 2.0
+ */
+
+namespace Pasarela\Bancomer\Controller\Payment;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Pasarela\Bancomer\Model\Payment as BancomerPayment;
+
+/**
+ * Webhook class  
+ */
+class Success extends \Magento\Framework\App\Action\Action
+{
+
+    protected $resultPageFactory;
+    protected $request;
+    protected $payment;
+    protected $checkoutSession;
+    protected $orderRepository;
+    protected $logger;
+    protected $_invoiceService;
+    protected $transactionBuilder;
+    
+    /**
+     * 
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @param \Magento\Framework\App\Request\Http $request
+     * @param BancomerPayment $payment
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Psr\Log\LoggerInterface $logger_interface
+     * @param \Magento\Sales\Model\Service\InvoiceService $invoiceService
+     */
+    public function __construct(
+            Context $context, 
+            PageFactory $resultPageFactory, 
+            \Magento\Framework\App\Request\Http $request, 
+            BancomerPayment $payment,
+            \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+            \Magento\Checkout\Model\Session $checkoutSession,
+            \Psr\Log\LoggerInterface $logger_interface,
+            \Magento\Sales\Model\Service\InvoiceService $invoiceService,
+            \Magento\Sales\Model\Order\Payment\Transaction\BuilderInterface $transactionBuilder
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+        $this->request = $request;
+        $this->payment = $payment;
+        $this->checkoutSession = $checkoutSession;
+        $this->orderRepository = $orderRepository;
+        $this->logger = $logger_interface;        
+        $this->_invoiceService = $invoiceService;
+        $this->transactionBuilder = $transactionBuilder;
+    }
+
+    /**
+     * Load the page defined in view/frontend/layout/bancomer_index_webhook.xml
+     * URL /openpay/payment/success
+     * 
+     * @url https://magento.stackexchange.com/questions/197310/magento-2-redirect-to-final-checkout-page-checkout-success-failed?rq=1
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute() {                
+        try {                        
+            echo "success";
+            
+        } catch (\Exception $e) {
+            $this->logger->error('#SUCCESS', array('message' => $e->getMessage(), 'code' => $e->getCode(), 'line' => $e->getLine(), 'trace' => $e->getTraceAsString()));
+            //throw new \Magento\Framework\Validator\Exception(__($e->getMessage()));
+        }
+        
+        return $this->resultRedirectFactory->create()->setPath('checkout/cart'); 
+    }
+}
--- app/code/Pasarela/Bancomer/Observer/AfterPlaceOrder.php
+++ app/code/Pasarela/Bancomer/Observer/AfterPlaceOrder.php
@@ -15,6 +15,24 @@ use Magento\Framework\DataObject;
 
 class AfterPlaceOrder implements ObserverInterface
 {
+
+    const SANDBOX = 'payment/pasarela_bancomer/is_sandbox';
+
+	const URL_SANDBOX = 'payment/pasarela_bancomer/url_sandbox_bancomer';
+
+	const URL_PRODUCCION = 'payment/pasarela_bancomer/url_produccion_bancomer';
+
+	const SANDBOX_MERCHANT_ID = 'payment/pasarela_bancomer/sandbox_merchant_id';
+
+	const SANDBOX_LLAVE_SECRETA = 'payment/pasarela_bancomer/sandbox_sk';
+
+    const SANDBOX_LLAVE_PUBLICA = 'payment/pasarela_bancomer/sandbox_pk';
+
+	const PRODUCCION_MERCHANT_ID = 'payment/pasarela_bancomer/live_merchant_id';
+
+	const PRODUCCION_LLAVE_SECRETA = 'payment/pasarela_bancomer/live_sk';
+
+    const PRODUCCION_LLAVE_PUBLICA = 'payment/pasarela_bancomer/live_pk';
     
     protected $config;
     protected $order;    
@@ -23,6 +41,11 @@ class AfterPlaceOrder implements ObserverInterface
     protected $_response;
     protected $_redirect;
     protected $openpayCustomerFactory;
+	
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
 
     public function __construct(
         Config $config, 
@@ -30,7 +53,8 @@ class AfterPlaceOrder implements ObserverInterface
         \Magento\Framework\App\Response\RedirectInterface $redirect,
         \Magento\Framework\App\ActionFlag $actionFlag,
         \Psr\Log\LoggerInterface $logger_interface,
-        \Magento\Framework\App\ResponseInterface $response
+        \Magento\Framework\App\ResponseInterface $response,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
     ) {
         $this->config = $config;
         $this->order = $order;        
@@ -39,7 +63,8 @@ class AfterPlaceOrder implements ObserverInterface
         $this->_redirect = $redirect;
         $this->_response = $response;
         
-        $this->_actionFlag = $actionFlag;                
+        $this->_actionFlag = $actionFlag;     
+        $this->scopeConfig = $scopeConfig;           
     }
     
     public function execute(Observer $observer) {
@@ -48,17 +73,52 @@ class AfterPlaceOrder implements ObserverInterface
                         
         if ($order->getPayment()->getMethod() != 'pasarela_bancomer') {
             return $this;
-        }        
+        }       
+
+		$storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+		$objectManager =  \Magento\Framework\App\ObjectManager::getInstance();     
+		$storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
+		//Se obtienen parametros de configuración por Store
+		$configData = $this->getConfigParams($storeScope, $storeManager->getStore()->getCode());
         
-        $charge = $this->config->getBancomerCharge($order->getExtOrderId(), $order->getExtCustomerId());
-        
-        $this->logger->debug('#AfterPlaceOrder', array('order_id' => $orderId[0], 'order_status' => $order->getStatus(), 'charge_id' => $charge->id, 'ext_order_id' => $order->getExtOrderId(), 'bancomer_status' => $charge->status));                                    
-        
-        if ($charge->status == 'charge_pending' && isset($_SESSION['bancomer_3d_secure_url'])) {               
-            $this->logger->debug('#AfterPlaceOrder', array('ext_order_id' => $order->getExtOrderId(), 'redirect_url' => $_SESSION['bancomer_3d_secure_url']));                    
-            $this->_actionFlag->set('', \Magento\Framework\App\Action\Action::FLAG_NO_DISPATCH, true);
-            $this->_redirect->redirect($this->_response, $_SESSION['bancomer_3d_secure_url']);            
-        }         
+        $this->logger->debug('#AfterPlaceOrder', array('order_id' => $orderId[0], 'order_status' => $order->getStatus(), 'charge_id' => $charge->id, 'ext_order_id' => $order->getExtOrderId(), 'bancomer_status' => $charge->status));            
+
+        echo 
+        '<form id="bancomermultipagos-form" method="post" action="'.$configData['url'].'">
+            <input type="hidden" name="mp_account" value="'.$configData['merchant_id'].'">
+            <input type="hidden" name="mp_order" value="'.$orderId[0].'">
+            <input type="hidden" name="mp_reference" value="'.$orderId[0].'">
+            <input type="hidden" name="mp_product" value="1">
+            <input type="hidden" name="mp_node" value="0">
+            <input type="hidden" name="mp_concept" value="2">
+            <input type="hidden" name="mp_amount" value="'.$order->getGrandTotal().'.00"><br>
+            <input type="hidden" name="mp_currency" value="1"><br>
+            URL Pago exitoso:<input type="hidden" name="mp_urlsuccess" value=" "><br>
+            URL Pago no exitoso:<input type="hidden" name="mp_urlfailure" value=" ">
+        </form>
+        <script type="text/javascript">
+            document.getElementById("bancomermultipagos-form").submit();
+        </script>';
     }    
+
+    //Obtiene los parámetros de configuración desde el cms
+    public function getConfigParams($storeScope, $websiteCode) 
+    {
+        $enviroment = $this->scopeConfig->getValue(self::SANDBOX, $storeScope, $websiteCode);
+        //Se valida entorno para obtener url del servicio
+        if($enviroment == '0'){
+            $configData['url'] = $this->scopeConfig->getValue(self::URL_SANDBOX, $storeScope, $websiteCode);
+            $configData['merchant_id'] = $this->scopeConfig->getValue(self::SANDBOX_MERCHANT_ID, $storeScope, $websiteCode);
+            $configData['secret_key'] = $this->scopeConfig->getValue(self::SANDBOX_LLAVE_SECRETA, $storeScope, $websiteCode);
+            $configData['public_key'] = $this->scopeConfig->getValue(self::SANDBOX_LLAVE_PUBLICA, $storeScope, $websiteCode);
+        } else{
+            $configData['url'] = $this->scopeConfig->getValue(self::URL_PRODUCCION, $storeScope, $websiteCode);
+            $configData['merchant_id'] = $this->scopeConfig->getValue(self::PRODUCCION_MERCHANT_ID, $storeScope, $websiteCode);
+            $configData['secret_key'] = $this->scopeConfig->getValue(self::PRODUCCION_LLAVE_SECRETA, $storeScope, $websiteCode);
+            $configData['public_key'] = $this->scopeConfig->getValue(self::PRODUCCION_LLAVE_PUBLICA, $storeScope, $websiteCode);
+        }
+        return $configData;
+
+    }
 
 }
