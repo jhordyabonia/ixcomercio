682dd8517c506f9c89cf7dfb7e7f7ea7155c2104
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-23 14:30:09 -0500

    Whitelabel - Se cambian metodos de captura 15

--- app/code/Pasarela/Bancomer/Model/Bancomer.php
+++ app/code/Pasarela/Bancomer/Model/Bancomer.php
@@ -10,70 +10,170 @@
 
 namespace Pasarela\Bancomer\Model;
 
-class Bancomer extends \Magento\Payment\Model\Method\Cc
+class Bancomer extends \Magento\Payment\Model\Method\AbstractMethod
 {
     const CODE = 'pasarela_bancomer';
 
     protected $_code = self::CODE;
-
-    protected $_canAuthorize = false;
-    protected $_canCapture = false;
-
-    /**
-    * Availability option
-    *
-    * @var bool
-    */
+    protected $_isGateway = true;
+    protected $_canAuthorize = true;
+    protected $_canOrder = true;
+    protected $_canCapture = true;
+    protected $_canCapturePartial = true;
+    protected $_canRefund = false;
+    protected $_canRefundInvoicePartial = false;
     protected $_isOffline = true;
+    protected $scope_config;
+    protected $openpay = false;
+    protected $is_sandbox;
+    protected $merchant_id = null;
+    protected $sk = null;
+    protected $deadline = 72;
+    protected $sandbox_merchant_id;
+    protected $sandbox_sk;
+    protected $live_merchant_id;
+    protected $live_sk;
+    protected $pdf_url_base;
+    protected $show_map;
+    protected $supported_currency_codes = array('MXN');
+    protected $_transportBuilder;
+    protected $logger;
+    protected $_storeManager;
+    protected $_inlineTranslation;
+    protected $_directoryList;
+    protected $_file;
+    
     /**
-     * Authorize payment abstract method
-     *
-     * @param \Magento\Framework\DataObject|InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @api
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * 
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
+     * @param \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory
+     * @param \Magento\Payment\Helper\Data $paymentData
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Payment\Model\Method\Logger $logger
+     * @param TransportBuilder $transportBuilder
+     * @param array $data
      */
-    public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        if (!$this->canAuthorize()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('The authorize action is not available.'));
+    public function __construct(
+            \Magento\Framework\Model\Context $context,
+            \Magento\Framework\Registry $registry, 
+            \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+            \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory, 
+            \Magento\Payment\Helper\Data $paymentData, 
+            \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig, 
+            \Magento\Payment\Model\Method\Logger $logger,             
+            \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
+            \Magento\Store\Model\StoreManagerInterface $storeManager,
+            \Psr\Log\LoggerInterface $logger_interface,
+            \Magento\Framework\App\Filesystem\DirectoryList $directoryList,
+            \Magento\Framework\Filesystem\Io\File $file,
+            array $data = []            
+    ) {
+        parent::__construct(
+            $context,
+            $registry, 
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData, 
+            $scopeConfig,
+            $logger,
+            null,
+            null,            
+            $data            
+        );
+    }
+
+    /**
+     * 
+     * @param \Magento\Payment\Model\InfoInterface $payment
+     * @param float $amount
+     * @return \Openpay\Stores\Model\Payment
+     * @throws \Magento\Framework\Validator\Exception
+     */
+    public function order(\Magento\Payment\Model\InfoInterface $payment, $amount) {
+
+        /**
+         * Magento utiliza el timezone UTC, por lo tanto sobreescribimos este 
+         * por la configuración que se define en el administrador         
+         */
+        $store_tz = $this->scope_config->getValue('general/locale/timezone');
+        date_default_timezone_set($store_tz);
+
+        /** @var \Magento\Sales\Model\Order $order */
+        $order = $payment->getOrder();
+
+        /** @var \Magento\Sales\Model\Order\Address $billing */
+        $billing = $order->getBillingAddress();
+
+        try {
+
+            $customer_data = array(
+                'name' => $billing->getFirstname(),
+                'last_name' => $billing->getLastname(),
+                'phone_number' => $billing->getTelephone(),
+                'email' => $order->getCustomerEmail()
+            );
+
+            if ($this->validateAddress($billing)) {
+                $customer_data['address'] = array(
+                    'line1' => $billing->getStreetLine(1),
+                    'line2' => $billing->getStreetLine(2),
+                    'postal_code' => $billing->getPostcode(),
+                    'city' => $billing->getCity(),
+                    'state' => $billing->getRegion(),
+                    'country_code' => $billing->getCountryId()
+                );
+            }
+
+            $due_date = date('Y-m-d\TH:i:s', strtotime('+ '.$this->deadline.' hours'));
+
+            $charge_request = array(
+                'method' => 'store',
+                'amount' => $amount,
+                'description' => sprintf('ORDER #%s, %s', $order->getIncrementId(), $order->getCustomerEmail()),
+                'order_id' => $order->getIncrementId(),
+                'due_date' => $due_date,
+                'customer' => $customer_data
+            );
+                        
+//            $pdf_file = $this->handlePdf($pdf_url, $order->getIncrementId());
+//            $this->sendEmail($pdf_file, $order);
+            
+        } catch (\Exception $e) {
+            $this->debugData(['exception' => $e->getMessage()]);
+            $this->_logger->error(__( $e->getMessage()));
+            throw new \Magento\Framework\Validator\Exception(__($this->error($e)));
         }
+
+        $payment->setSkipOrderProcessing(true);
         return $this;
     }
+
     /**
-     * Capture payment abstract method
-     *
-     * @param \Magento\Framework\DataObject|InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @api
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @param Address $billing
+     * @return boolean
      */
-    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        if (!$this->canCapture()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('The capture action is not available.'));
+    public function validateAddress($billing) {
+        if ($billing->getStreetLine(1) && $billing->getCity() && $billing->getPostcode() && $billing->getRegion() && $billing->getCountryId()) {
+            return true;
         }
-        return $this;
+        return false;
     }
-    /**
-     * Refund specified amount for payment
-     *
-     * @param \Magento\Framework\DataObject|InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @api
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+    
+    
+    /*
+     * Validate if host is secure (SSL)
      */
-    public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        if (!$this->canRefund()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('The refund action is not available.'));
+    public function hostSecure() {
+        $is_secure = false;
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+            $is_secure = true;
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+            $is_secure = true;
         }
-        return $this;
+        
+        return $is_secure;
     }
+
 }
