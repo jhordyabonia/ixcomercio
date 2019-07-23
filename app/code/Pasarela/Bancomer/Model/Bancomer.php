84965719afb4d95e0b359c4dfdebbdc7f116dc4f
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-23 09:12:18 -0500

    Whitelabel - SE agregan datos de configuración

--- app/code/Pasarela/Bancomer/Model/Bancomer.php
+++ app/code/Pasarela/Bancomer/Model/Bancomer.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Pasarela_Bancomer payment method model
+ *
+ * @category    Pasarela
+ * @package     Pasarela_Bancomer
+ * @author      Valentina Aguirre
+ * @license     http://www.apache.org/licenses/LICENSE-2.0  Apache License Version 2.0
+ */
 
 namespace Pasarela\Bancomer\Model;
 
--- app/code/Pasarela/Bancomer/view/frontend/web/js/view/payment/method-renderer/bancomer.js
+++ app/code/Pasarela/Bancomer/view/frontend/web/js/view/payment/method-renderer/bancomer.js
@@ -1,3 +1,11 @@
+/**
+ * Pasarela_Bancomer Magento JS component
+ *
+ * @category    Bancomer
+ * @package     Pasarela_Bancomer
+ * @author      Valentina Aguirre
+ * @license     http://www.apache.org/licenses/LICENSE-2.0  Apache License Version 2.0
+ */
 define([
         'jquery',
         'Magento_Checkout/js/view/payment/default'
