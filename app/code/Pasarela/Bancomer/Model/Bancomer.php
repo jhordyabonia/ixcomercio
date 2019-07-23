7cdb02e1e85970d2caae628ab05cb562fda4ee72
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-23 11:11:07 -0500

    Whitelabel - Se cambian metodos de captura

--- app/code/Pasarela/Bancomer/Model/Bancomer.php
+++ app/code/Pasarela/Bancomer/Model/Bancomer.php
@@ -18,126 +18,55 @@ class Bancomer extends \Magento\Payment\Model\Method\Cc
 
     protected $_canAuthorize = true;
     protected $_canCapture = true;
-
     /**
-     * Capture Payment.
+     * Authorize payment abstract method
      *
-     * @param \Magento\Payment\Model\InfoInterface $payment
+     * @param \Magento\Framework\DataObject|InfoInterface $payment
      * @param float $amount
      * @return $this
-     */
-    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        try {
-            //check if payment has been authorized
-            if(is_null($payment->getParentTransactionId())) {
-                $this->authorize($payment, $amount);
-            }
-
-            //build array of payment data for API request.
-            $request = [
-                'capture_amount' => $amount,
-                //any other fields, api key, etc.
-            ];
-
-            //make API request to credit card processor.
-            $response = $this->makeCaptureRequest($request);
-
-            //todo handle response
-
-            //transaction is done.
-            $payment->setIsTransactionClosed(1);
-
-        } catch (\Exception $e) {
-            $this->debug($payment->getData(), $e->getMessage());
-        }
-
-        return $this;
-    }
-
-    /**
-     * Authorize a payment.
-     *
-     * @param \Magento\Payment\Model\InfoInterface $payment
-     * @param float $amount
-     * @return $this
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @api
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount)
     {
-        try {
-
-            ///build array of payment data for API request.
-            $request = [
-                'cc_type' => $payment->getCcType(),
-                'cc_exp_month' => $payment->getCcExpMonth(),
-                'cc_exp_year' => $payment->getCcExpYear(),
-                'cc_number' => $payment->getCcNumberEnc(),
-                'amount' => $amount
-            ];
-
-            //check if payment has been authorized
-            $response = $this->makeAuthRequest($request);
-
-        } catch (\Exception $e) {
-            $this->debug($payment->getData(), $e->getMessage());
+        if (!$this->canAuthorize()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The authorize action is not available.'));
         }
-
-        if(isset($response['transactionID'])) {
-            // Successful auth request.
-            // Set the transaction id on the payment so the capture request knows auth has happened.
-            $payment->setTransactionId($response['transactionID']);
-            $payment->setParentTransactionId($response['transactionID']);
-        }
-
-        //processing is not done yet.
-        $payment->setIsTransactionClosed(0);
-
         return $this;
     }
-
     /**
-     * Set the payment action to authorize_and_capture
+     * Capture payment abstract method
      *
-     * @return string
-     */
-    public function getConfigPaymentAction()
-    {
-        return self::ACTION_AUTHORIZE_CAPTURE;
-    }
-
-    /**
-     * Test method to handle an API call for authorization request.
-     *
-     * @param $request
-     * @return array
+     * @param \Magento\Framework\DataObject|InfoInterface $payment
+     * @param float $amount
+     * @return $this
      * @throws \Magento\Framework\Exception\LocalizedException
+     * @api
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function makeAuthRequest($request)
+    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
     {
-        $response = ['transactionId' => 123]; //todo implement API call for auth request.
-
-        if(!$response) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('Failed auth request.'));
+        if (!$this->canCapture()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The capture action is not available.'));
         }
-
-        return $response;
+        return $this;
     }
-
     /**
-     * Test method to handle an API call for capture request.
+     * Refund specified amount for payment
      *
-     * @param $request
-     * @return array
+     * @param \Magento\Framework\DataObject|InfoInterface $payment
+     * @param float $amount
+     * @return $this
      * @throws \Magento\Framework\Exception\LocalizedException
+     * @api
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function makeCaptureRequest($request)
+    public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount)
     {
-        $response = ['success']; //todo implement API call for capture request.
-
-        if(!$response) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('Failed capture request.'));
+        if (!$this->canRefund()) {
+            throw new \Magento\Framework\Exception\LocalizedException(__('The refund action is not available.'));
         }
-
-        return $response;
+        return $this;
     }
 }
--- app/code/Pasarela/Bancomer/README.md
+++ app/code/Pasarela/Bancomer/README.md
@@ -1,2 +1,2 @@
-# LlamaCoin
+# Bancomer
 Magento 2 Custom Payment Method Proof of Concept
