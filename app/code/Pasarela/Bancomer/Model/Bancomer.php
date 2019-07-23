8fa2cd9acdde0fb0b1dbd9197dc2829b7de66b71
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-23 14:13:46 -0500

    Whitelabel - Se cambian metodos de captura 9

--- app/code/Pasarela/Bancomer/Model/Bancomer.php
+++ app/code/Pasarela/Bancomer/Model/Bancomer.php
@@ -10,62 +10,11 @@
 
 namespace Pasarela\Bancomer\Model;
 
-class Bancomer extends \Magento\Payment\Model\Method\Cc
+class Bancomer extends \Magento\Payment\Model\Method\AbstractMethod
 {
     const CODE = 'pasarela_bancomer';
 
     protected $_code = self::CODE;
 
     protected $_isOffline = true;
-    /**
-     * Authorize payment abstract method
-     *
-     * @param \Magento\Framework\DataObject|InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @api
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function authorize(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        if (!$this->canAuthorize()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('The authorize action is not available.'));
-        }
-        return $this;
-    }
-    /**
-     * Capture payment abstract method
-     *
-     * @param \Magento\Framework\DataObject|InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @api
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        if (!$this->canCapture()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('The capture action is not available.'));
-        }
-        return $this;
-    }
-    /**
-     * Refund specified amount for payment
-     *
-     * @param \Magento\Framework\DataObject|InfoInterface $payment
-     * @param float $amount
-     * @return $this
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @api
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount)
-    {
-        if (!$this->canRefund()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('The refund action is not available.'));
-        }
-        return $this;
-    }
 }
