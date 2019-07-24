a82bcea7a3ef3722f05c9e50f3629d844caa8f9d
Valentina <valentina.aguirre@ariadnacg.com>
2019-07-24 08:59:54 -0500

    Whitelabel - Se corrige payload para placeorder 1

--- app/code/Trax/Ordenes/Observer/PlaceOrder.php
+++ app/code/Trax/Ordenes/Observer/PlaceOrder.php
@@ -176,14 +176,16 @@ class PlaceOrder implements \Magento\Framework\Event\ObserverInterface
             'TaxRegistrationNumber' => "64251 2 357348 DV41",
             'InvoiceRequested' => true,
             'ReceiveInvoiceByMail' => true,
-            'Shipments' => [array(
-                'FreightService' => 'mienvio',
-                'FreightShipmentId' => '123456789',
-                'ServiceType' => $order->getShippingMethod(),
-                'CarrierId' => 'CLL3',
-                'Amount' => $order->getShippingAmount(),
-                'FreightCost' => $order->getShippingAmount(),
-            )],
+            'Shipments' => array(
+                array(
+                    'FreightService' => 'mienvio',
+                    'FreightShipmentId' => '123456789',
+                    'ServiceType' => $order->getShippingMethod(),
+                    'CarrierId' => 'CLL3',
+                    'Amount' => $order->getShippingAmount(),
+                    'FreightCost' => $order->getShippingAmount(),
+                )
+            ),
             'Items' => $items
         );
         $payload = json_encode($payload);
