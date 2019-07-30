35e5c4d0901eac89ff5509c369709d1f84b46723
valen92 <valen92_@hotmail.com>
2019-07-29 19:24:40 -0500

    Se agrega template para cancelar ordenes 1

---
+++ app/code/Pasarela/Bancomer/Block/Error.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pasarela\Bancomer\Block;
+class Error extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+}
