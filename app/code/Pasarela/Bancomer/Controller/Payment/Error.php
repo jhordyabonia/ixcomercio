8eee056728d88c545132b031ab848ffa3530d6b1
valen92 <valen92_@hotmail.com>
2019-07-29 19:21:15 -0500

    Se agrega template para cancelar ordenes

--- app/code/Pasarela/Bancomer/Controller/Payment/Error.php
+++ app/code/Pasarela/Bancomer/Controller/Payment/Error.php
@@ -17,6 +17,27 @@ use Magento\Framework\View\Result\PageFactory;
 class Error extends \Magento\Framework\App\Action\Action
 {
 
+    const API_KEY = 'trax_general/catalogo_retailer/apikey';
+
+	const ACCESS_KEY = 'trax_general/catalogo_retailer/accesskey';
+
+	const ENVIROMENT = 'trax_general/catalogo_retailer/apuntar_a';
+
+	const URL_DESARROLLO = 'trax_general/catalogo_retailer/url_desarrollo';
+
+	const URL_PRODUCCION = 'trax_general/catalogo_retailer/url_produccion';
+
+    const ORDENES_REINTENTOS = 'trax_general/ordenes_general/ordenes_reintentos';
+
+    const ORDENES_CORREO = 'trax_general/ordenes_general/ordenes_correo';
+    
+    private $helper;
+	
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
     protected $resultPageFactory;
     protected $request;
     protected $payment;
@@ -25,17 +46,22 @@ class Error extends \Magento\Framework\App\Action\Action
     protected $logger;
     protected $_invoiceService;
     protected $transactionBuilder;
+    protected $_iwsOrder;
     
     /**
      * 
      * @param Context $context
      * @param PageFactory $resultPageFactory
      * @param \Magento\Framework\App\Request\Http $request
-     * @param BancomerPayment $payment
      * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Psr\Log\LoggerInterface $logger_interface
      * @param \Magento\Sales\Model\Service\InvoiceService $invoiceService
+     * @param \Magento\Sales\Model\Order\Payment\Transaction\BuilderInterface $transactionBuilder
+     * @param \Pasarela\Bancomer\Model\BancomerTransaccionesFactory  $bancomerTransacciones
+     * @param \Magento\Framework\Controller\ResultFactory $result
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Trax\Catalogo\Helper\Email $email
      */
     public function __construct(
             Context $context, 
@@ -45,7 +71,12 @@ class Error extends \Magento\Framework\App\Action\Action
             \Magento\Checkout\Model\Session $checkoutSession,
             \Psr\Log\LoggerInterface $logger_interface,
             \Magento\Sales\Model\Service\InvoiceService $invoiceService,
-            \Magento\Sales\Model\Order\Payment\Transaction\BuilderInterface $transactionBuilder
+            \Magento\Sales\Model\Order\Payment\Transaction\BuilderInterface $transactionBuilder,
+            \Pasarela\Bancomer\Model\BancomerTransaccionesFactory  $bancomerTransacciones,
+            \Magento\Framework\Controller\ResultFactory $result,
+            \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+            \Trax\Catalogo\Helper\Email $email,
+            \Trax\Ordenes\Model\IwsOrderFactory $iwsOrder
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
@@ -55,6 +86,11 @@ class Error extends \Magento\Framework\App\Action\Action
         $this->logger = $logger_interface;        
         $this->_invoiceService = $invoiceService;
         $this->transactionBuilder = $transactionBuilder;
+        $this->_bancomerTransacciones = $bancomerTransacciones;
+        $this->resultRedirect = $result;
+        $this->scopeConfig = $scopeConfig;
+        $this->helper = $email;
+        $this->_iwsOrder = $iwsOrder;
     }
 
     /**
@@ -65,15 +101,64 @@ class Error extends \Magento\Framework\App\Action\Action
      * @return \Magento\Framework\View\Result\Page
      */
     public function execute() {                
-        try {                        
-            echo "error";
-            exit();
-            
+        try {               
+            /*$mp_order = $_POST['mp_order'];
+            $mp_reference = $_POST['mp_reference'];
+            $mp_amount = $_POST['mp_amount'];
+            $mp_response = $_POST['mp_response'];
+            $mp_authorization = $_POST['mp_authorization'];
+            $mp_paymentMethod = $_POST['mp_paymentMethod'];
+            $mp_cardType = $_POST['mp_cardType'];
+            $mp_response = $_POST['mp_response'];
+            $mp_date = $_POST['mp_date'];
+            $mp_paymentMethodCode = $_POST['mp_paymentMethodCode'];
+            $mp_signature = $_POST['mp_signature'];
+            $mp_bankname = $_POST['mp_bankname'];
+            $mp_bankcode = $_POST['mp_bankcode'];
+            $mp_pan = $_POST['mp_pan'];
+            $mp_saleid = $_POST['mp_saleid'];
+            $mp_signature1 = hash('sha256', $mp_order.$mp_reference.$mp_amount.'.00'.$mp_authorization);*/
+            $mp_order = "56";
+            $mp_reference = "2000000086";
+            $mp_amount = "133070,89";
+            $mp_paymentMethod = "TDX";
+            $mp_cardType = "credito";
+            $mp_response = "0";
+            $mp_responsemsg = "Transacción autorizada";
+            $mp_authorization = "abc123";
+            $mp_date = "2019-07-25 16:15:15";
+            $mp_paymentMethodCode = "123";
+            $mp_bankname = "Marvel Bank";
+            $mp_bankcode = "BANXICO";
+            $mp_saleid = "1";
+            $mp_pan = "12345678";
+            $mp_signature = hash('sha256', $mp_order.$mp_reference.$mp_amount.'.00'.$mp_authorization);
+            $mp_signature1 = hash('sha256', $mp_order.$mp_reference.$mp_amount.'.00'.$mp_authorization);
+            $resultPage = $this->resultPageFactory->create();
+            $resultPage->getLayout()->initMessages();
+            //TODO: Cancelar orden
+            $this->cancelOrder($mp_order);
+            $resultPage->getLayout()->getBlock('bancomer_success')->setTitle("Transacción Cancelada");         
+            $resultPage->getLayout()->getBlock('bancomer_success')->setOrder($mp_order);
+            $resultPage->getLayout()->getBlock('bancomer_success')->setReference($mp_reference);
+            $resultPage->getLayout()->getBlock('bancomer_success')->setAmount($mp_amount);
+            $resultPage->getLayout()->getBlock('bancomer_success')->setPaymentMethod($mp_paymentMethod);
+            $resultPage->getLayout()->getBlock('bancomer_success')->setResponse($mp_responsemsg);
         } catch (\Exception $e) {
             $this->logger->error('#SUCCESS', array('message' => $e->getMessage(), 'code' => $e->getCode(), 'line' => $e->getLine(), 'trace' => $e->getTraceAsString()));
-            //throw new \Magento\Framework\Validator\Exception(__($e->getMessage()));
+            $resultPage->getLayout()->getBlock('bancomer_success')->setTitle("Error");
         }
-        
-        return $this->resultRedirectFactory->create()->setPath('checkout/cart'); 
+        return $resultPage;
+    }
+    
+    //Se cambia estado de la orden y se cancela
+    public function cancelOrder($mp_order){   
+        try {
+            $order = $this->orderRepository->get((int)$mp_order);           
+            $order->cancel();   
+            $this->logger->info('RegisterPayment - Se cancela orden');     
+        } catch(Exception $e){
+            $this->logger->info('RegisterPayment - Se ha producido un error: '.$e->getMessage());
+        }
     }
 }
