238086d4e43334e8930a4943a16c6b5d20f7f633
Valentina <valentina.aguirre@ariadnacg.com>
2019-08-26 10:45:39 -0500

    IWS - Se corrige error en nombre de productos

--- app/code/Trax/Catalogo/Observer/GetCartProducts.php
+++ app/code/Trax/Catalogo/Observer/GetCartProducts.php
@@ -200,14 +200,40 @@ class GetCartProducts implements \Magento\Framework\Event\ObserverInterface
         $products = $productFactory->create();
         $product = $products->loadByAttribute('sku', $catalog->Sku);
         if($product){
-            $url=strtolower($catalog->Description.'-'.$catalog->Sku);
+            $url=strtolower($catalog->Description.'-'.$catalog->Sku.'-'.$storeId.'-'.rand(0,1000));
             $cleanurl = html_entity_decode(strip_tags($url));
             $product->setUrlKey($cleanurl);
-            $product->setName($catalog->Description); // Name of Product
+            $iwsDescription = explode("- ", $catalog->Description);
+            $name = $iwsDescription[0];
+            $description = "";
+            if(isset($iwsDescription[1])){
+                $name .= $iwsDescription[1];
+            }
+            if(isset($iwsDescription[2])){
+                $name .= $iwsDescription[2];
+                for($i = 3; $i < count($iwsDescription); $i++){
+                    $description .= $iwsDescription[$i];
+                }
+            }        
+            $categoryIds = array_unique(
+                array_merge(
+                    $product->getCategoryIds(),
+                    $categoryIds
+                )
+            );    
+            $websiteIds = array_unique(
+                array_merge(
+                    $product->getWebsiteIds(),
+                    array($websiteId)
+                )
+            );
+            $product->setCategoryIds($categoryIds);
+            $product->setName($name); // Name of Product        
+            $product->setDescription($description); // Description of Product
             $product->setAttributeSetId($configData['attribute_id']); // Attribute set id
+            $product->setWebsiteIds($websiteIds);
+            $this->logger->info('GetProducts - Se asocia website a producto: '.$websiteId);
             $product->setStatus(1); // Status on product enabled/ disabled 1/0
-            $product->setStoreId($storeId);
-            $product->setWeight(10); // weight of product
             $product->setVisibility(4); // visibilty of product (catalog / search / catalog, search / Not visible individually)
             $product->setTaxClassId($configData['tax_id']); // Tax class id
             switch($catalog->Type){
@@ -224,28 +250,16 @@ class GetCartProducts implements \Magento\Framework\Event\ObserverInterface
                     $product->setTypeId('downloadable');
                     break;
             } // type of product (simple/virtual/downloadable/configurable)
-            $product->setPrice($catalog->Price->UnitPrice);
-            if($catalog->InStock == 0){
-                $stock = 0;
-            } else {
-                $stock = 1;
-            }
-            $product->setStockData(
-                array(
-                    'use_config_manage_stock' => 0,
-                    'manage_stock' => 1,
-                    'is_in_stock' => $stock,
-                    'min_sale_qty' => 1,
-                    'qty' => $catalog->InStock
-                )
-            );
             //Set product dimensions
             if(isset($catalog->Freight)){
                 if(isset($catalog->Freight->Package)){
                     $product->setWeight($catalog->Freight->Package->Weight);
-                    $product->setLength($catalog->Freight->Package->Length);
-                    $product->setWidth($catalog->Freight->Package->Width);
-                    $product->setHeight($catalog->Freight->Package->Height);
+                    $product->setData('length',$catalog->Freight->Package->Length);
+                    $product->setData('ts_dimensions_length',$catalog->Freight->Package->Length);
+                    $product->setData('width',$catalog->Freight->Package->Width);
+                    $product->setData('ts_dimensions_width',$catalog->Freight->Package->Width);
+                    $product->setData('height',$catalog->Freight->Package->Height);
+                    $product->setData('ts_dimensions_height',$catalog->Freight->Package->Height);
                 }
             }
             try{
