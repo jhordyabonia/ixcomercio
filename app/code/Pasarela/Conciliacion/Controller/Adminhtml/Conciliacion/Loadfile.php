7e336b9ddd69dbdb0e71f73aa4503abf6f5a9f87
Valentina <valentina.aguirre@ariadnacg.com>
2019-09-06 14:59:28 -0500

    BANCOMER - Se añade render form 8

---
+++ app/code/Pasarela/Conciliacion/Controller/Adminhtml/Conciliacion/Loadfile.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Conciliacion Admin Cagegory Map Record Loadfile Controller.
+ * @category  Pasarela
+ * @package   Pasarela_Conciliacion
+ * @author    Valentina Aguirre
+ * @copyright Copyright (c) 2010-2016 Pasarela Software Private Limited (https://webkul.com)
+ * @license   https://store.webkul.com/license.html
+ */
+namespace Pasarela\Conciliacion\Controller\Adminhtml\Conciliacion;
+
+class Loadfile extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Pasarela\Conciliacion\Model\ConciliacionFactory
+     */
+    var $gridFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Pasarela\Conciliacion\Model\ConciliacionFactory $gridFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Pasarela\Conciliacion\Model\ConciliacionFactory $gridFactory
+    ) {
+        parent::__construct($context);
+        $this->gridFactory = $gridFactory;
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function execute()
+    {
+        echo "entra"; exit();
+        $data = $this->getRequest()->getPostValue();
+        if (!$data) {
+            $this->_redirect('pasarela_conciliacion/conciliacion/addrow');
+            return;
+        }
+        try {
+            $rowData = $this->gridFactory->create();
+            $rowData->setData($data);
+            if (isset($data['id'])) {
+                $rowData->setEntityId($data['id']);
+            }
+            $rowData->save();
+            $this->messageManager->addSuccess(__('Payment method has been successfully saved.'));
+        } catch (\Exception $e) {
+            $this->messageManager->addError(__($e->getMessage()));
+        }
+        $this->_redirect('pasarela_conciliacion/conciliacion/index');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Pasarela_Conciliacion::save');
+    }
+}
