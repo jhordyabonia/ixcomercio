74abfc901d31fe6ec3a56d3b2246faeea0001f7b
Valentina <valentina.aguirre@ariadnacg.com>
2019-09-09 17:59:18 -0500

    BANCOMER - Se agregan validaciones a carga de archivo 12

--- app/code/Pasarela/Conciliacion/Controller/Adminhtml/Conciliacion/Loadfile.php
+++ app/code/Pasarela/Conciliacion/Controller/Adminhtml/Conciliacion/Loadfile.php
@@ -116,14 +116,21 @@ class Loadfile extends Action
     
     public function validateFile($filePath)
     {
+        
         $this->logger->info('BANCOMER - entra a función: '.$filePath);
         $fila = 1;
-        $this->logger->info('BANCOMER - sigue if: '.$filePath);
         if (($gestor = fopen($filePath, "r")) !== FALSE) {
-            $this->logger->info('BANCOMER - sigue while: '.$filePath);
+            while (($datos = fgetcsv($gestor, 1000, ";")) !== FALSE) {
+                $numero = count($datos);
+                $this->logger->info('BANCOMER - '.$numero.' de campos en la línea '.$fila);
+                $fila++;
+                for ($c=0; $c < $numero; $c++) {
+                    $this->logger->info('BANCOMER - Datos: '.$datos[$c]);
+                }
+            }
             fclose($gestor);
-            $this->logger->info('BANCOMER - finaliza while: '.$filePath);
         }
+
     }
  
     public function getDestinationPath()
