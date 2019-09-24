848cfb38f7ae2e83b5f24b1c1de3d4dbb4a864e5
Valentina <valentina.aguirre@ariadnacg.com>
2019-09-24 15:26:39 -0500

    IWS - Se agrega valor de impuesto configuable 5

--- app/code/Trax/Ordenes/Observer/PlaceOrder.php
+++ app/code/Trax/Ordenes/Observer/PlaceOrder.php
@@ -228,7 +228,7 @@ class PlaceOrder implements \Magento\Framework\Event\ObserverInterface
         $orderItems = $order->getAllItems();
         $coupon = array();
         $shippingAmount = $order->getShippingAmount() - ($order->getShippingAmount() * $configDataImpuesto / 100);
-        $this->logger->info('PlaceOrder - Valor envio: '.$order->getShippingAmount().' Valor sin impuesto: '.$shippingAmount);
+        $this->logger->info('PlaceOrder - Valor envio: '.$order->getShippingAmount().' Valor sin impuesto: '.$shippingAmount.' Porcentaje impuesto: '.$configDataImpuesto);
         if($order->getCouponCode() != '' || $order->getCouponCode() != null){            
             $coupon = array($order->getCouponCode());
         }
