092a503645115dada53c93c0f7b58d666e870e25
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-10-09 10:29:07 -0500

    account - city field - v.1.1.4

--- app/design/frontend/Cdi/marley/web/src/js/main.js
+++ app/design/frontend/Cdi/marley/web/src/js/main.js
@@ -203,7 +203,7 @@ function ($, Component) {
     // =============================================
     // Get cities
     // =============================================
-    /*
+    
     var fieldState = $('form .fieldset > .field.region #region_id');
 
     fieldState.on('change', function (e) {
@@ -217,7 +217,7 @@ function ($, Component) {
         }
       });      
     });
-    */
+    
 
 
   });
