f2a442ecd53455af418974c6f432bc8362fec7fe
Valentina <valentina.aguirre@ariadnacg.com>
2019-11-18 17:13:04 -0500

    Se genera módulo para conectarse al api de mi envio y recibir notificaciones

---
+++ app/code/Mienvio/Api/Setup/UpgradeData.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Mienvio\Api\Setup;
+
+use Magento\Eav\Model\Config;
+use Magento\Eav\Setup\EavSetup; 
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var Config
+     */
+    private $eavConfig;
+
+     /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * @param Config $eavConfig
+     * @param EavSetupFactory $eavSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
+    public function __construct(
+        Config $eavConfig,
+        EavSetupFactory $eavSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    ) {
+        $this->eavConfig            = $eavConfig;
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->attributeSetFactory  = $attributeSetFactory;
+    }
+
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context){
+        $setup->startSetup();
+		if(version_compare($context->getVersion(), '0.0.1', '<')){
+            $conn = $setup->getConnection();
+            $setup->getConnection()->addColumn(
+				$setup->getTable( 'iws_order' ),
+				'mienvio_guide',
+				[
+					'type' => \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+					'nullable' => false,
+					'default' => 0,
+					'comment' => 'Indicates if the guide has been generated',
+					'after' => 'iws_order'
+				]
+			);
+            $setup->getConnection()->addColumn(
+				$setup->getTable( 'iws_order' ),
+				'mienvio_delivery',
+				[
+					'type' => \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+					'nullable' => false,
+					'default' => 0,
+					'comment' => 'Indicates if the order has been delivered',
+					'after' => 'mienvio_guide'
+				]
+			);
+		}	
+        $setup->endSetup();
+    }
+}
