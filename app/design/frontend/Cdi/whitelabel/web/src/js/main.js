1cd4b82cedc1cb770bfea9da343d8f44a9a669a6
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-11-20 12:12:46 -0500

    WL - get places v.1.0.2

--- app/design/frontend/Cdi/whitelabel/web/src/js/main.js
+++ app/design/frontend/Cdi/whitelabel/web/src/js/main.js
@@ -206,6 +206,32 @@ require(['jquery', 'owlCarouselJs', 'mainJs', 'domReady!'], function($) {
 		});
 
 
+		// =============================================
+	    // Get cities
+	    // =============================================
+	    
+	    var fieldCity = $('form .fieldset > .field.city #city_id');
+
+	    fieldState.on('change', function (e) {
+	      	$.ajax({
+		        url: '/places/search/',
+		        data: 'parentId='+fieldState.find('option:selected').attr('parentId'),
+		        type: 'GET',
+		        dataType: 'json',
+		        success: function(res) {
+			        $(fieldCity).find('option:not([value=""])').remove();
+			        $.each(res, function(i, val){
+			            $(fieldCity).append("<option value='"+val.Id+"'>"+val.Name+"</option>");
+			    	});
+		        }
+	      	});
+
+	      	var valState = $(fieldState).find('option:selected');
+			$(fieldState).parent().find('input').val($(valState).text());
+			$(fieldState).parent().find('input').keyup();
+	    });
+
+
 
 	});
 
