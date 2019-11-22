128910ea0e94738652f7fe904516a880f7c3e73c
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-11-22 12:21:39 -0500

    Zendesk v.1.0.3

--- app/design/frontend/Cdi/lvgt/web/src/js/main.js
+++ app/design/frontend/Cdi/lvgt/web/src/js/main.js
@@ -418,7 +418,7 @@ require(['jquery', 'owlCarouselJs', 'mainJs', 'domReady!'], function($) {
 	    $.each(linksFooter, function(i, val){
 	    	if(val.innerText == "Zendesk Support" || val.innerText == "Preguntas frecuentes"){
 	        	var parentLi = $(this).parent();
-	        	$('footer .col-md-3:eq(0) ul').append(parentLi);
+	        	$('footer .col-sm-3:eq(0) ul').append(parentLi);
 	      	}
 	    });
 
