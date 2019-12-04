9338b6fb85a6b1a5d5f2e73d66cc254c720fcb30
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-12-04 06:31:46 -0500

    jam header mobile v.1.2.2

--- app/design/frontend/Cdi/custom/web/src/js/main.js
+++ app/design/frontend/Cdi/custom/web/src/js/main.js
@@ -20,7 +20,7 @@ function ($, Component) {
 	    var htmlSearchWrapper = $('header .block-search');
 	    if(w_width <= 768){
 	    	$('.skip-links-wrapper').append($(htmlCartWrapper).html());
-	    	$('header').append(htmlSearchWrapper);
+	    	//$('header').append(htmlSearchWrapper);
 	    }
 	
 		
