45a74400b53f2b71f275e29d0effa6b54d0d2145
Luis Maldonado <luis.maldonado@ariadnacg.com>
2019-12-23 13:56:51 -0500

    Hp menu mobile v.1.4.4

--- app/design/frontend/Cdi/marley/web/src/js/main.js
+++ app/design/frontend/Cdi/marley/web/src/js/main.js
@@ -9,7 +9,64 @@ function ($, Component) {
   var statusField;
   var totalItemsMinicart = $('.block-minicart .product-item');
 
+  var countAjaxComplete = 0;
+  
+
   $(document).ready(function(){
+
+
+    function updateShoppingCart(){
+    var itemsCart = $('.cart.table-wrapper .items>.item .col.subtotal .price');
+    var subtotal = 0;
+    $.each(itemsCart, function(i, val){
+      var item = $(val).text().split(/\s+/);
+      $.each(item, function(iItem, valItem){
+        var price = (valItem).replace(/\./g, '');
+          price = (price).replace(/\,/g, '.');
+        if($.isNumeric(parseFloat(price)) == true){
+          subtotal += parseFloat(price);
+        }
+      });
+    });
+    console.log(subtotal);
+    console.log(Number(subtotal).toFixed(2));
+
+
+    var orderSubtotal = $('.totals.sub .price').text().split(/\s+/);
+    var summarySubtotal = 0;
+    $.each(orderSubtotal, function(i, val){
+        var summaryPriceSubtotal = (val).replace(/\./g, '');
+          summaryPriceSubtotal = (summaryPriceSubtotal).replace(/\,/g, '.');
+        if($.isNumeric(summaryPriceSubtotal) == true){
+            summarySubtotal += parseFloat(summaryPriceSubtotal);
+        }
+    });
+    console.log(summarySubtotal);
+    console.log(Number(summarySubtotal).toFixed(2));
+
+
+    var orderTax = $('.totals-tax-summary .price').text().split(/\s+/);
+    var summaryTax = 0;
+    $.each(orderTax, function(i, val){
+    var summaryPriceTax = (val).replace(/\./g, '');
+          summaryPriceTax = (summaryPriceTax).replace(/\,/g, '.');
+        if($.isNumeric(summaryPriceTax) == true){
+            summaryTax += parseFloat(summaryPriceTax);
+        }
+    });
+    console.log(summaryTax);
+    console.log(Number(summaryTax).toFixed(2));
+    console.log(Number(summarySubtotal+summaryTax).toFixed(2));
+
+
+    if(Number(subtotal).toFixed(2) != Number(summarySubtotal+summaryTax).toFixed(2)){
+      console.log("dif"); 
+    }else{
+      console.log("sim");
+    }
+  }
+
+  
 
     totalItemsMinicart=totalItemsMinicart.length;
 
@@ -459,58 +516,6 @@ function ($, Component) {
 
   });
 
-
-  var countAjaxComplete = 0;
-  function updateShoppingCart(){
-    var itemsCart = $('.cart.table-wrapper .items>.item .col.subtotal .price');
-    var subtotal = 0;
-    $.each(itemsCart, function(i, val){
-      var item = $(val).text().split(/\s+/);
-      $.each(item, function(iItem, valItem){
-        var price = (valItem).replace(/\./g, '');
-          price = (price).replace(/\,/g, '.');
-        if($.isNumeric(parseFloat(price)) == true){
-          subtotal += parseFloat(price);
-        }
-      });
-    });
-    console.log(subtotal);
-    console.log(Number(subtotal).toFixed(2));
-
-
-    var orderSubtotal = $('.totals.sub .price').text().split(/\s+/);
-    var summarySubtotal = 0;
-    $.each(orderSubtotal, function(i, val){
-        var summaryPriceSubtotal = (val).replace(/\./g, '');
-          summaryPriceSubtotal = (summaryPriceSubtotal).replace(/\,/g, '.');
-        if($.isNumeric(summaryPriceSubtotal) == true){
-            summarySubtotal += parseFloat(summaryPriceSubtotal);
-        }
-    });
-    console.log(summarySubtotal);
-    console.log(Number(summarySubtotal).toFixed(2));
-
-
-    var orderTax = $('.totals-tax-summary .price').text().split(/\s+/);
-    var summaryTax = 0;
-    $.each(orderTax, function(i, val){
-    var summaryPriceTax = (val).replace(/\./g, '');
-          summaryPriceTax = (summaryPriceTax).replace(/\,/g, '.');
-        if($.isNumeric(summaryPriceTax) == true){
-            summaryTax += parseFloat(summaryPriceTax);
-        }
-    });
-    console.log(summaryTax);
-    console.log(Number(summaryTax).toFixed(2));
-    console.log(Number(summarySubtotal+summaryTax).toFixed(2));
-
-
-    if(Number(subtotal).toFixed(2) != Number(summarySubtotal+summaryTax).toFixed(2)){
-      console.log("dif"); 
-    }else{
-      console.log("sim");
-    }
-  }
   
   $(document).ajaxComplete(function(){
     if($('.totals.sub .price').text() != ""){
