dffda8233c33c6ad7bd30d6f9347e99758a08f28
Luis Maldonado <luis.maldonado@ariadnacg.com>
2020-01-02 14:41:55 -0500

    zone field v.1.0.5

--- app/design/frontend/Cdi/marley/web/src/js/main.js
+++ app/design/frontend/Cdi/marley/web/src/js/main.js
@@ -250,31 +250,33 @@ function ($, Component) {
     // =============================================
 
     var fieldCity = $('form .fieldset > .field.city #city_id');
+    var fieldZoneStreet = $('form .fieldset > .field.street-zone .control');
+    /*
     var fieldStreet = $('form .fieldset > .field.street .control .nested .additional .control');
     var htmlStreet = '<select id="fieldSelectStreet" class="select" name="street2_id" aria-required="true" aria-invalid="false">'+
           '<option data-title="" value="">Please select a zone.</option>'+
           '</select>';
     $(fieldStreet).append(htmlStreet);
-      $(fieldStreet).find('input').hide();
+    $(fieldStreet).find('input').hide();*/
 
-      fieldState.on('change', function (e) {
-          $.ajax({
-            url: '/places/search/',
-            data: 'parentId='+fieldState.find('option:selected').attr('parentId'),
-            type: 'GET',
-            dataType: 'json',
-            success: function(res) {
-              $(fieldCity).find('option:not([value=""])').remove();
-              $.each(res, function(i, val){
-                  $(fieldCity).append("<option value='"+val.Id+"' parentId='"+val.Id+"'>"+val.Name+"</option>");
-            });
-            }
-          });
+    fieldState.on('change', function (e) {
+      $.ajax({
+        url: '/places/search/',
+        data: 'parentId='+fieldState.find('option:selected').attr('parentId'),
+        type: 'GET',
+        dataType: 'json',
+        success: function(res) {
+          $(fieldCity).find('option:not([value=""])').remove();
+          $.each(res, function(i, val){
+              $(fieldCity).append("<option value='"+val.Id+"' parentId='"+val.Id+"'>"+val.Name+"</option>");
+        });
+        }
+      });
 
-          var valState = $(fieldState).find('option:selected');
+      var valState = $(fieldState).find('option:selected');
       $(fieldState).parent().find('input').val($(valState).text());
       $(fieldState).parent().find('input').keyup();
-      });
+    });
 
 
       // =============================================
@@ -288,9 +290,9 @@ function ($, Component) {
         type: 'GET',
         dataType: 'json',
         success: function(resCity) {
-          $(fieldStreet).find('select option:not([value=""])').remove();
+          $(fieldZoneStreet).find('select option:not([value=""])').remove();
             $.each(resCity, function(iResCity, valResCity){
-              $(fieldStreet).find('select').append("<option value='"+valResCity.ParentId+"' parentId='"+valResCity.ParentId+"' postalCode='"+valResCity.PostalCode+"'>"+valResCity.Name+"</option>");
+              $(fieldZoneStreet).find('select').append("<option value='"+valResCity.ParentId+"' parentId='"+valResCity.ParentId+"' postalCode='"+valResCity.PostalCode+"'>"+valResCity.Name+"</option>");
             });
         }
       });
