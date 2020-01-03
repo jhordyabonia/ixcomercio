d47064f4b924fbd7e6c5ecbeb9b92a01547cc7ea
Valentina <valentina.aguirre@ariadnacg.com>
2020-01-03 18:05:42 -0500

    Se imprimen datos relacionados con shipping 6

--- app/code/Mienvio/Api/Controller/Shipment/Status.php
+++ app/code/Mienvio/Api/Controller/Shipment/Status.php
@@ -152,7 +152,7 @@ class Status extends \Magento\Framework\App\Action\Action implements CsrfAwareAc
                                 $configData = $this->getConfigParams($storeScope, $storeManager->getStore()->getCode());
                                 $mienvio_data = $this->loadMienvioData($configData, $order->getMienvioQuoteId());
                                 if(count($mienvio_data['resp']->purchase->shipments)>0){
-                                    $mienvio_data = $mienvio_data->purchase->shipments[0];
+                                    $mienvio_data = $mienvio_data['resp']->purchase->shipments[0];
                                 }
                                 $resultPage->getLayout()->getBlock('mienvio_status')->setMienvioData($mienvio_data);
                             }
