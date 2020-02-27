6ec395d3eed261075dfef360fc8a4ba07cf3a789
Jhonatan Holguín <jholguin9004@gmail.com>
2020-02-27 09:09:45 -0500

    limpia código postal cuando no es numérico

---
+++ app/code/Cdi/Custom/Model/ShippingInformationManagement.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Cdi\Custom\Model;
+
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\StateException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\Data\AddressInterface;
+use Magento\Quote\Api\Data\CartInterface;
+use Psr\Log\LoggerInterface as Logger;
+use Magento\Quote\Model\QuoteAddressValidator;
+use Magento\Quote\Api\Data\CartExtensionFactory;
+use Magento\Quote\Model\ShippingAssignmentFactory;
+use Magento\Quote\Model\ShippingFactory;
+use Magento\Framework\App\ObjectManager;
+
+class ShippingInformationManagement extends \Magento\Checkout\Model\ShippingInformationManagement
+{
+
+    public function saveAddressInformation(
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    ) {
+        /** @var \Magento\Quote\Model\Quote $quote */
+        $quote = $this->quoteRepository->getActive($cartId);
+        $this->validateQuote($quote);
+
+        $address = $addressInformation->getShippingAddress();
+        if (!$address || !$address->getCountryId()) {
+            throw new StateException(__('The shipping address is missing. Set the address and try again.'));
+        }
+        if (!$address->getCustomerAddressId()) {
+            $address->setCustomerAddressId(null);
+        }
+
+        try {
+            $billingAddress = $addressInformation->getBillingAddress();
+            if ($billingAddress) {
+                if (!$billingAddress->getCustomerAddressId()) {
+                    $billingAddress->setCustomerAddressId(null);
+                }
+                $this->addressValidator->validateForCart($quote, $billingAddress);
+                $quote->setBillingAddress($billingAddress);
+            }
+
+            $this->addressValidator->validateForCart($quote, $address);
+            $carrierCode = $addressInformation->getShippingCarrierCode();
+            $address->setLimitCarrier($carrierCode);
+            $methodCode = $addressInformation->getShippingMethodCode();
+            $quote = $this->prepareShippingAssignment($quote, $address, $carrierCode . '_' . $methodCode);
+
+            $quote->setIsMultiShipping(false);
+
+            $this->quoteRepository->save($quote);
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            throw new InputException(
+                __('The shipping information was unable to be saved. Verify the input data and try again.')
+            );
+        }
+
+        $shippingAddress = $quote->getShippingAddress();
+        if(!is_numeric($shippingAddress->getPostcode())) $shippingAddress->setPostcode('')->save();
+        $this->logger->error($shippingAddress->getPostcode());
+
+        if (!$shippingAddress->getShippingRateByCode($shippingAddress->getShippingMethod())) {
+            throw new NoSuchEntityException(
+                __('Carrier with such method not found: %1, %2', $carrierCode, $methodCode)
+            );
+        }
+
+        /** @var \Magento\Checkout\Api\Data\PaymentDetailsInterface $paymentDetails */
+        $paymentDetails = $this->paymentDetailsFactory->create();
+        $paymentDetails->setPaymentMethods($this->paymentMethodManagement->getList($cartId));
+        $paymentDetails->setTotals($this->cartTotalsRepository->get($cartId));
+        return $paymentDetails;
+    }
+
+    public function beforeSaveAddressInformation($subject, $cartId, $addressInformation)
+    {
+        $shippingAddress = $addressInformation->getShippingAddress();
+        // Do something
+
+        return null;
+    }
+
+    private function prepareShippingAssignment(CartInterface $quote, AddressInterface $address, $method)
+    {
+        $cartExtension = $quote->getExtensionAttributes();
+        if ($cartExtension === null) {
+            $cartExtension = $this->cartExtensionFactory->create();
+        }
+
+        $shippingAssignments = $cartExtension->getShippingAssignments();
+        if (empty($shippingAssignments)) {
+            $shippingAssignment = $this->shippingAssignmentFactory->create();
+        } else {
+            $shippingAssignment = $shippingAssignments[0];
+        }
+
+        $shipping = $shippingAssignment->getShipping();
+        if ($shipping === null) {
+            $shipping = $this->shippingFactory->create();
+        }
+
+        $shipping->setAddress($address);
+        $shipping->setMethod($method);
+        $shippingAssignment->setShipping($shipping);
+        $cartExtension->setShippingAssignments([$shippingAssignment]);
+        return $quote->setExtensionAttributes($cartExtension);
+    }
+}
