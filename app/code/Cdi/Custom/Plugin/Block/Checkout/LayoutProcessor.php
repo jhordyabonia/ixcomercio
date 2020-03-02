c061442092b9f50c516072cb5c64c5ef33d4432a
Jhonatan Holguín <jholguin9004@gmail.com>
2020-03-02 16:47:07 -0500

    prueba de modfificación de orden de campos en billing

--- app/code/Cdi/Custom/Plugin/Block/Checkout/LayoutProcessor.php
+++ app/code/Cdi/Custom/Plugin/Block/Checkout/LayoutProcessor.php
@@ -9,12 +9,16 @@ class LayoutProcessor
 
     public function afterProcess(MagentoLayoutProcessor $subject, $jsLayout)
     {
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
+        $logger = new \Zend\Log\Logger();
+        $logger->addWriter($writer);
+        $logger->info(print_r($jsLayout, true));
+        /*
         if (isset($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']
         ['payment']['children']['payments-list']['children']
         )){
             foreach ($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']
                 ['payment']['children']['payments-list']['children'] as $key => $payment) {
-                /* company */
                 if (isset($payment['children']['form-fields']['children']['company'])) {
                     $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']
                     ['payment']['children']['payments-list']['children'][$key]['children']['form-fields']['children']
@@ -22,6 +26,7 @@ class LayoutProcessor
                 }
             }
         }
+        */
         return $jsLayout;
     }
 }
