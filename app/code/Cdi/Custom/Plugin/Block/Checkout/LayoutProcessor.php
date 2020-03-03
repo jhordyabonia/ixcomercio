0a9f96902669deb36c70d5798012c49f7c7fe9d8
Jhonatan Holguín <jholguin9004@gmail.com>
2020-03-03 10:10:27 -0500

    establece orden de los campos en billing address

--- app/code/Cdi/Custom/Plugin/Block/Checkout/LayoutProcessor.php
+++ app/code/Cdi/Custom/Plugin/Block/Checkout/LayoutProcessor.php
@@ -1,7 +1,7 @@
 <?php
 namespace Cdi\Custom\Plugin\Block\Checkout;
 
-use Magento\Checkout\Block\Checkout\LayoutProcessor as MageLayoutProcessor;
+use Magento\Checkout\Block\Checkout\LayoutProcessor as MagentoLayoutProcessor;
 
 
 class LayoutProcessor
@@ -9,24 +9,32 @@ class LayoutProcessor
 
     public function afterProcess(MagentoLayoutProcessor $subject, $jsLayout)
     {
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
-        $logger = new \Zend\Log\Logger();
-        $logger->addWriter($writer);
-        $logger->info(print_r($jsLayout, true));
-        /*
         if (isset($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']
         ['payment']['children']['payments-list']['children']
         )){
-            foreach ($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']
-                ['payment']['children']['payments-list']['children'] as $key => $payment) {
-                if (isset($payment['children']['form-fields']['children']['company'])) {
-                    $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']
-                    ['payment']['children']['payments-list']['children'][$key]['children']['form-fields']['children']
-                    ['company']['sortOrder'] = 10;
+            $fields = array(
+                'firstname' => 20,
+                'lastname' => 25,
+                'telephone' => 30,
+                'company' => 35,
+                'country_id' => 40,
+                'region' => 45,
+                'region_id' => 46,
+                'city' => 90,
+                'street' => 95,
+                'zone_id' => 100,
+                'postcode' => 110
+            );
+            foreach($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'] as $key => $payment){
+                foreach($fields as $field => $sort){
+                    if (isset($payment['children']['form-fields']['children'][$field])) {
+                        $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']
+                        ['payment']['children']['payments-list']['children'][$key]['children']['form-fields']['children']
+                        [$field]['sortOrder'] = $sort;
+                    }
                 }
             }
         }
-        */
         return $jsLayout;
     }
 }
