f2c6b32a5fc4ed010c45c35a02adffbf9169e98c
Javier Gomez Patiño <xcbjagom@intcomex.com>
2020-03-06 11:40:48 -0500

    ajuste script

--- app/code/Pasarela/Bancomer/Block/Success.php
+++ app/code/Pasarela/Bancomer/Block/Success.php
@@ -2,12 +2,51 @@
 namespace Pasarela\Bancomer\Block;
 class Success extends \Magento\Framework\View\Element\Template
 {
-	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	protected $_orderCollectionFactory; # GTM add xcb
+
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory ) # GTM add xcb
 	{
+		$this->_orderCollectionFactory = $orderCollectionFactory; # GTM add xcb
 		parent::__construct($context);
 	}
 
-	public function imprimir(){
-		echo 'algo';
+	/*
+	Add to class for get order details
+	xcb GTM
+	*/
+	public function getOrderCollection(){
+		$collection = $this->_orderCollectionFactory->create()
+         ->addAttributeToSelect('*')
+		 ->addFieldToFilter($field, $condition); //Add condition if you wish
+		 return $collection;
+	}
+	
+	/*
+	Add to class for get order details
+	xcb GTM 
+	*/
+	public function getOrderCollectionByCustomerId($customerId){
+       $collection = $this->_orderCollectionFactory()->create($customerId)
+         ->addFieldToSelect('*')
+         ->addFieldToFilter('status',
+                ['in' => $this->_orderConfig->getVisibleOnFrontStatuses()]
+            )
+         ->setOrder(
+                'created_at',
+                'desc'
+            );
+ 
+     return $collection;
+
+    }
+	
+	/*
+	Add to class for get order details
+	xcb GTM 
+	*/
+	public function getStoreName(){
+		return $this->_storeManager->getStore()->getName();
 	}
 }
