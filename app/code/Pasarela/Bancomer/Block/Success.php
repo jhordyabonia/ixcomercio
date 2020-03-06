d921641030ec149459a4acd670ca76b0371dee98
Javier Gomez Patiño <xcbjagom@intcomex.com>
2020-03-06 13:39:23 -0500

    ajuste script

--- app/code/Pasarela/Bancomer/Block/Success.php
+++ app/code/Pasarela/Bancomer/Block/Success.php
@@ -2,27 +2,27 @@
 namespace Pasarela\Bancomer\Block;
 class Success extends \Magento\Framework\View\Element\Template
 {
-	protected $_orderCollectionFactory; # GTM add xcb
+	//protected $_orderCollectionFactory; # GTM add xcb
 
 	public function __construct(
-		\Magento\Framework\View\Element\Template\Context $context,
-		\Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory ) # GTM add xcb
+		\Magento\Framework\View\Element\Template\Context $context
+		/*,\Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory */) # GTM add xcb
 	{
-		$this->_orderCollectionFactory = $orderCollectionFactory; # GTM add xcb
+		//$this->_orderCollectionFactory = $orderCollectionFactory; # GTM add xcb
 		parent::__construct($context);
 	}
 
 	/*
 	Add to class for get order details
 	xcb GTM
-	*/
+	
 	public function getOrderCollection(){
 		$collection = $this->_orderCollectionFactory->create()
          ->addAttributeToSelect('*')
 		 ->addFieldToFilter($field, $condition); //Add condition if you wish
 		 return $collection;
 	}
-	
+	*/
 	/*
 	Add to class for get order details
 	xcb GTM 
