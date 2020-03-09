daa8a226a21ad4f1d65ab0cb298e8699419b1d27
Luis Maldonado <luis.maldonado@ariadnacg.com>
2020-03-09 14:42:09 -0500

    #91 Modificar nombre de pestaña Mas Información v.1.0.3

--- app/design/frontend/Cdi/lvgt/web/src/js/main.js
+++ app/design/frontend/Cdi/lvgt/web/src/js/main.js
@@ -427,12 +427,16 @@ require(['jquery', 'owlCarouselJs', 'mainJs', 'domReady!'], function($) {
 	    function getStatesCheckout(obj){
 	    	$('body').trigger('processStart');
 
+	    	$(obj).find('input[name="postcode"]').parents('.field').hide();
+	    	$(obj).find('input[name="postcode"]').val('');
+
+	    	/*
 	    	if($(obj).find("select[name='country_id']").val() == "GT"){
 	    		$(obj).find('input[name="postcode"]').parents('.field').hide();
 	    	}else{
 	    		$(obj).find('input[name="postcode"]').parents('.field').show();
 	    	}
-	    	$(obj).find('input[name="postcode"]').val('');
+	    	*/
 
 	    	var fieldStreetCheckout = $(obj).find('> .field.street .control .additional .control');
 	    	var fieldZoneCheckout = $(obj).find('> .field input[name="custom_attributes[zone_id]"]').parent();
@@ -558,11 +562,13 @@ require(['jquery', 'owlCarouselJs', 'mainJs', 'domReady!'], function($) {
 						$(fieldStreetCheckout).find('input').keyup();
 
 				    	if($(valStreetCheckout).attr('postalCode') != 'null'){
-							$('input[name="postcode"]').val($(valStreetCheckout).attr('postalCode'));
-				    		$('input[name="postcode"]').keyup();
+				    		$(obj).find('input[name="postcode"]').show();
+							$(obj).find('input[name="postcode"]').val($(valStreetCheckout).attr('postalCode'));
+				    		$(obj).find('input[name="postcode"]').keyup();
 						}else{
-							$('input[name="postcode"]').val($(valStreetCheckout).text());
-				    		$('input[name="postcode"]').keyup();
+							$(obj).find('input[name="postcode"]').hide();
+							$(obj).find('input[name="postcode"]').val($(valStreetCheckout).text());
+				    		$(obj).find('input[name="postcode"]').keyup();
 						}
 
 				    	$('body').trigger('processStop');
