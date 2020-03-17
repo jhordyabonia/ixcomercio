b39d98aafe951486364c9fb05590e1abbf3fc063
Luis Maldonado <luis.maldonado@ariadnacg.com>
2020-03-17 16:43:30 -0500

    test translate v.1.0.0

--- app/design/frontend/Cdi/lvgt/web/src/js/main.js
+++ app/design/frontend/Cdi/lvgt/web/src/js/main.js
@@ -2,11 +2,11 @@ require(['jquery', 'mage/translate', 'owlCarouselJs', 'mainJs', 'domReady!'], fu
     
 	jQuery(document).ready(function() {
 
-		$(html).append("<span data-bind=\"i18n: \'Text to translate\'\">"+$.mage.__("Text to translate 1")+"</span><br>");
-		$(html).append("<span data-bind=\"i18n: \'Text to translate\'\">Text to translate 2</span><br>");
-		$(html).append("<span translate=\"\'Text to translate\'\">"+$.mage.__("Text to translate 3")+"</span><br>");
-		$(html).append("<span translate=\"\'Text to translate\'\">Text to translate 4</span><br>");
-		$(html).append("<span>"+$.mage.__('<string>')+"</span><br>");
+		$('html').append("<span data-bind=\"i18n: \'Text to translate\'\">"+$.mage.__("Text to translate 1")+"</span><br>");
+		$('html').append("<span data-bind=\"i18n: \'Text to translate\'\">Text to translate 2</span><br>");
+		$('html').append("<span translate=\"\'Text to translate\'\">"+$.mage.__("Text to translate 3")+"</span><br>");
+		$('html').append("<span translate=\"\'Text to translate\'\">Text to translate 4</span><br>");
+		$('html').append("<span>"+$.mage.__('<string>')+"</span><br>");
 
 		var w_width = $( window ).width();
 		var w_height = $( window ).height();
