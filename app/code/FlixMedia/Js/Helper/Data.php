74ce2be4369bc7280c6b34804f81a43debb421c7
sergio.daza <sergio.daza@intcomex.com>
2020-04-06 14:19:41 -0500

    Config Data Admin

--- app/code/FlixMedia/Js/Helper/Data.php
+++ app/code/FlixMedia/Js/Helper/Data.php
@@ -5,12 +5,22 @@ use Magento\Framework\App\Helper\AbstractHelper;
 
 class Data extends AbstractHelper{
 
+	const ID = 'flix_general/catalogo_retailer/apikey';
+	const LANGUAGE = 'flix_general/catalogo_retailer/language';
+
 	protected $_coreRegistry;
+
+	/**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
 	
 	public function __construct(
-		\Magento\Catalog\Block\Product\Context $productContext
+		\Magento\Catalog\Block\Product\Context $productContext,
+		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
 	){
-		$this->_coreRegistry = $productContext->getRegistry();	
+		$this->_coreRegistry = $productContext->getRegistry();
+		$this->scopeConfig = $scopeConfig;	
 	}
 	
 	/**
@@ -20,5 +30,14 @@ class Data extends AbstractHelper{
 	public function getProduct()
     {
         return $this->_coreRegistry->registry('product');
-    }
+	}
+	
+	//Obtiene los parámetros de configuración desde el cms
+    public function getConfigParams() 
+    {
+        //Se obtienen parametros de configuración por Store
+        $configData['id'] = $this->scopeConfig->getValue(self::ID);
+		$configData['language'] = $this->scopeConfig->getValue(self::LANGUAGE);
+		return $configData;
+	}
 }
