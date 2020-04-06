a47fe5b69bacb1f57c592ebb58331e8e15bea9fd
Luis Maldonado <luis.maldonado@ariadnacg.com>
2020-04-06 15:38:51 -0500

    data form v.1.3.4

--- app/design/frontend/Cdi/whitelabel/web/js/main.min.js
+++ app/design/frontend/Cdi/whitelabel/web/js/main.min.js
@@ -697,6 +697,8 @@ require(['jquery', 'owlCarouselJs', 'jquery/ui', 'mage/translate', 'mainJs', 'do
 		        if($(this).prop('checked') == false){
 			        fieldStateCheckout = $('.billing-address-form form fieldset.address input[name="region"]').parent();
 			        if($(fieldStateCheckout).length >= 1 && flagBillingForm == 1){
+			        	var inputsRemove = $('.billing-address-form form fieldset.address').find('#fieldStateCheckout, #fieldCityCheckout, #fieldZoneCheckout');
+			        	$(inputsRemove).remove();
 	                	getStatesCheckout($('.billing-address-form form fieldset.address'), '> .field input[name="custom_attributes[zone_id]"]');
 			        }
 			    }
