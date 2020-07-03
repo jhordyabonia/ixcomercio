be23793819ff7b9c10709c1e67579c434843924e
German Dario Cardenas Patiño <xcbgcard@intcomex.com>
2020-07-03 12:38:45 -0500

    Comandos para ejecutar procesos del catalogo: productos, precios y stock

--- app/code/Trax/Catalogo/Console/Commands.php
+++ app/code/Trax/Catalogo/Console/Commands.php
@@ -5,9 +5,28 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\Console\Cli;
+use Trax\Catalogo\Cron\GetStock;
+use Trax\Catalogo\Cron\GetCatalog;
+
+
+/**
+* @var Trax\Catalogo\Cron\GetStock
+*/
+private $_stock;
+
+/**
+* @var Trax\Catalogo\Cron\GetCatalog
+*/
+private $_catalog;
  
 class Commands extends Command 
 {
+   public function __construct(GetStock $stock, GetCatalog $catalog)
+   {
+        $this->_stock   = $stock;
+        $this->_catalog = $catalog;
+   }
+
    protected  function configure()
    {
         $options = [
@@ -27,22 +46,20 @@ class Commands extends Command
 
    protected  function execute(InputInterface $input, OutputInterface $output)
    {
-
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $name = $input->getOption(self::NAME);
+
+        $output->writeln("Starting Get Trax " . $name . " " .  date('Y-m-d H:i:s'));
 
         switch($name){
             case 'stock':
-                $entityManager = $objectManager->get('\Trax\Catalogo\Cron\GetStock');
+                $this->_stock->execute();
             break;
             case 'catalog':
-                $entityManager = $objectManager->get('\Trax\Catalogo\Cron\GetCatalog');
+                $this->_catalog->execute();
             break;
         }
 
-        $output->writeln("Starting Get Trax " . $name . " " .  date('Y-m-d H:i:s'));
-        $entityManager->execute();
-        $output->writeln("Ending Get Trax " $name . " " . date('Y-m-d H:i:s'));
+        $output->writeln("Ending Get Trax " $name . " " . date('Y-m-d H:i:s')); 
         
         return Cli::RETURN_SUCCESS;
     }
