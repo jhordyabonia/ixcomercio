0050937fccd2e6f24c8efb115e3bfc900a2c11c9
Rafael Ferrer <xcbrferr@intcomex.com>
2020-07-21 15:03:37 -0500

    Header subsección por Familia dvlp-REQ-{#14394-V2}

---
+++ app/code/Intcomex/CategoryTitle/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category    Events
+ * @package     Intcomex_CategoryTitle
+ * @author      Sergio Daza
+ * @license     http://www.apache.org/licenses/LICENSE-2.0  Apache License Version 2.0
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Intcomex_CategoryTitle',
+    __DIR__
+);
