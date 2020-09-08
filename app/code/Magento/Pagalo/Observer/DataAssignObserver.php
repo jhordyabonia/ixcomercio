0a6d707fb6e563b3f7de6607a7800b06245de237
Rafael Ferrer <rafael.ferrer@itncomex.com>
2020-09-08 09:20:59 -0500

    Ajustes Visanet dvlp-REQ-{#15417}

--- app/code/Magento/Pagalo/Observer/DataAssignObserver.php
+++ app/code/Magento/Pagalo/Observer/DataAssignObserver.php
@@ -21,16 +21,18 @@ class DataAssignObserver extends AbstractDataAssignObserver
         $data = $this->readDataArgument($observer);
 
         $additionalData = $data->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
+        
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/pagalo_debug.log');
+        $this->logger = new \Zend\Log\Logger();
+        $this->logger->addWriter($writer);
+        $this->logger->info(print_r($additionalData,true));
+
         if (!is_array($additionalData)) {
             return;
         }
 
         $paymentInfo = $this->readPaymentModelArgument($observer);
 
-        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/pagalo_debug.log');
-        $this->logger = new \Zend\Log\Logger();
-        $this->logger->addWriter($writer);
-        $this->logger->info(print_r($additionalData,true));
 
 
         foreach ($additionalData as $key => $value) {
