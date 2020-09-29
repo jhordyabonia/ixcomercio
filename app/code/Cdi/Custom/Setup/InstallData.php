593a95797549480ddc8811219e662bb13758c26a
Rafael Ferrer <rafael.ferrer@intcomex.com>
2020-09-29 18:06:46 -0500

    Ajustes stock dvlp-REQ-{#15914}

--- app/code/Cdi/Custom/Setup/InstallData.php
+++ app/code/Cdi/Custom/Setup/InstallData.php
@@ -9,46 +9,59 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 
 class InstallData implements InstallDataInterface
 {
-	private $eavSetupFactory;
+        /**
+        * EAV setup factory
+        *
+        * @var EavSetupFactory
+        */
+        private $eavSetupFactory;
 
-	public function __construct(EavSetupFactory $eavSetupFactory)
-	{
-		$this->eavSetupFactory = $eavSetupFactory;
-	}
-	
-	public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
-	{
-		$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        /**
+        * Init
+        *
+        * @param EavSetupFactory $eavSetupFactory
+        */
+        public function __construct(EavSetupFactory $eavSetupFactory)
+        {
+        $this->eavSetupFactory = $eavSetupFactory;
+        }
 
-		$statusOptions = [
-			['label' => __('Si'), 'value' => 1],
-			['label' => __('No'), 'value' => 0]
-		];
+        /**
+        * {@inheritdoc}
+        * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+        */
+        public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+        {
+        /* @var EavSetup $eavSetup /
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
 
-		$eavSetup->addAttribute(
-			\Magento\Catalog\Model\Product::ENTITY,
-			'activate_from_stock',
-			[
-                'type' => 'int',
-                'backend' => '',
-                'frontend' => '',
-                'label' => '',
-                'input' => 'select',
-                'class' => '',
-                'source' => $statusOptions,
-                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
-                'visible' => true,
-                'required' => false,
-                'user_defined' => false,
-                'default' => '1',
-                'searchable' => false,
-                'filterable' => false,
-                'comparable' => false,
-                'is_used_in_grid' => true,
-                'visible_on_front' => false,
-                'used_in_product_listing' => true,
-                'unique' => false
-            ]
-		);
-	}
+        /**
+        * Add attributes to the eav/attribute
+        */
+        $eavSetup->removeAttribute(\Magento\Catalog\Model\Product::ENTITY, 'call_for_price_active');
+        $eavSetup->addAttribute(
+        \Magento\Catalog\Model\Product::ENTITY, 'text_demo',
+        [
+
+        'type' => 'varchar',
+        'backend' => '',
+        'frontend' => '',
+        'label' => 'Text Demo',
+        'input' => 'boolean',
+        'class' => '',
+        'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+        'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+        'visible' => true,
+        'required' => false,
+        'user_defined' => false,
+        'default' => 0,
+        'searchable' => false,
+        'filterable' => false,
+        'comparable' => false,
+        'visible_on_front' => false,
+        'used_in_product_listing' => true,
+        'unique' => false
+        ]
+        );
+        }
 }
