4c582fbf91b1321dff940e5f4c25d5b00cd1c85f
Johan Martinez <johan.martinez@intcomex.com>
2020-10-22 15:07:49 -0500

    Actualiza clase Export csv REQ #16165

--- app/code/Cdi/Custom/Controller/Index/Export.php
+++ app/code/Cdi/Custom/Controller/Index/Export.php
@@ -20,6 +20,52 @@ use Magento\ImportExport\Model\Export\Entity\ExportInfoFactory;
 class Export extends ExportController implements HttpPostActionInterface
 {
     /**
+     * @var \Magento\Framework\App\Response\Http\FileFactory
+     */
+    protected $fileFactory;
+
+    /**
+     * @var \Magento\Framework\Session\SessionManagerInterface
+     */
+    private $sessionManager;
+
+    /**
+     * @var PublisherInterface
+     */
+    private $messagePublisher;
+
+    /**
+     * @var ExportInfoFactory
+     */
+    private $exportInfoFactory;
+
+    /**
+     * @param Context $context
+     * @param FileFactory $fileFactory
+     * @param \Magento\Framework\Session\SessionManagerInterface|null $sessionManager
+     * @param PublisherInterface|null $publisher
+     * @param ExportInfoFactory|null $exportInfoFactory
+     */
+    public function __construct(
+        Context $context,
+        FileFactory $fileFactory,
+        \Magento\Framework\Session\SessionManagerInterface $sessionManager = null,
+        PublisherInterface $publisher = null,
+        ExportInfoFactory $exportInfoFactory = null
+    ) {
+        $this->fileFactory = $fileFactory;
+        $this->sessionManager = $sessionManager ?: \Magento\Framework\App\ObjectManager::getInstance()
+            ->get(\Magento\Framework\Session\SessionManagerInterface::class);
+        $this->messagePublisher = $publisher ?: \Magento\Framework\App\ObjectManager::getInstance()
+            ->get(PublisherInterface::class);
+        $this->exportInfoFactory = $exportInfoFactory ?:
+            \Magento\Framework\App\ObjectManager::getInstance()->get(
+                ExportInfoFactory::class
+            );
+        parent::__construct($context);
+    }
+
+    /**
      * Load data with filter applying and create file for download.
      *
      * @return \Magento\Backend\Model\View\Result\Redirect
