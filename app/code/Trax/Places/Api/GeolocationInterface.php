2fe53c332894f878cf66b862722d215b36bada1b
Johan Martinez <johan.martinez@intcomex.com>
2020-11-09 15:54:51 -0500

    Add api geolocation REQ #16162

---
+++ app/code/Trax/Places/Api/GeolocationInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Trax\Places\Api;
+
+interface GeolocationInterface
+{
+    /**
+     * GET for Post api
+     * @param string $value
+     * @return string
+     */
+ 
+    public function getPost($value);
+}
---
+++ app/code/Trax/Places/Model/Api/Geolocation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Trax\Places\Model\Api;
+
+use Psr\Log\LoggerInterface;
+
+class Geolocation
+{
+    protected $logger;
+
+    public function __construct(
+        LoggerInterface $logger
+    )
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     *  @inheritdoc
+     */
+
+    public function getPost($value)
+    {
+        $response = ['success' => false];
+
+        try {
+            // Your Code here
+            $response = ['success' => true, 'message' => $value];
+        }catch (\Exception $e) {
+            $response = ['success' => false, 'message' => $e->getMessage()];
+            $this->logger->info($e->getMessage());
+        }
+        $returnArray = json_encode($response);
+        return $returnArray;
+    }
+
+}
