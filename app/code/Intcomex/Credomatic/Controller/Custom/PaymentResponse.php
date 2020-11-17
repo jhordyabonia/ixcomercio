33a2d69bcd4187884902da9d73495969ebf8694a
Rafael Ferrer <rafael.ferrer@intcomex.com>
2020-11-17 04:11:49 -0500

    Integracion Credomatic dvlp-REQ-{#15418}

---
+++ app/code/Intcomex/Credomatic/Controller/Custom/PaymentResponse.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Intcomex\Credomatic\Controller\Custom;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class PaymentResponse extends \Magento\Framework\App\Action\Action
+{
+
+    protected $resultRedirect;
+    protected $_messageManager;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\Controller\ResultFactory $resultPageFactory,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Framework\Message\ManagerInterface $messageManager 
+    ) {
+        parent::__construct($context);
+        $this->_scopeConfig = $scopeConfig;
+        $this->resultRedirect = $context->getResultFactory();
+        $this->_checkoutSession = $checkoutSession;
+        $this->_messageManager = $messageManager;
+        
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute(){ 
+        ini_set('display_errors', 1);
+        try {
+
+            $objectManager =  \Magento\Framework\App\ObjectManager::getInstance(); 
+            $customError = (string) $this->_scopeConfig->getValue('payment/Credomatic/CustomErrorMsg');
+            $showCustomError = false;
+            if($customError != '') {
+                $showCustomError = true;
+            }
+
+            $body = $_GET;
+            echo '<pre>';
+            print_r($body);
+            echo '</pre>';
+
+            $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/credomatic_trans_resp.log');
+            $this->logger = new \Zend\Log\Logger();
+            $this->logger->addWriter($writer);
+            $this->logger->info(print_r($body,true));
+            
+            if($body['response_code']==100||$body['response_code']==200){
+                if( $showCustomError ) {
+                    $msgError = $showCustomError;
+                }else {
+                    $msgError = $body['responsetext'];
+                }
+                $this->_checkoutSession->setErrorMessage($msgError);
+                $this->_messageManager->addError($msgError);
+                $resultRedirect = $this->resultRedirectFactory->create();
+                $resultRedirect->setPath('checkout');
+
+            }else if($body['response_code']==1000){
+                $order = $objectManager->create('\Magento\Sales\Model\OrderRepository')->get($body['orderid']);
+                $payment = $order->getPayment();
+                $payment->setLastTransId($body['transactionid']);
+                $resultRedirect = $this->resultRedirectFactory->create();
+                $resultRedirect->setPath('checkout/onepage/success');
+            }
+            return $resultRedirect;
+        } catch (\Exception $e) {
+            $error = __('Payment create data error Credomatic: '); 
+            throw new \Magento\Framework\Validator\Exception(__($error.$e->getMessage())); 
+        }
+        
+    }
+
+}
---
+++ app/code/Intcomex/Credomatic/view/frontend/web/js/view/payment/method-renderer/stripe-method.js
@@ -0,0 +1,83 @@
+/**
+ * Credomatic for Magento JS component
+ *
+ * @category    Credomatic
+ * @package     Credomatic
+ * @author      Intcomex
+ * @copyright   Intcomex (https://www.intcomex.com/)
+ */
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'ko',
+        'Intcomex_Credomatic/js/view/payment/cc-form',
+        'jquery',
+        'Magento_Payment/js/model/credit-card-validation/validator',
+        'Magento_Checkout/js/model/quote',
+        'mage/storage',
+        'mage/url',
+        'Magento_Checkout/js/action/get-payment-information',
+
+    ],
+    function (ko,Component, $,setPaymentMethodAction,quote,storage,url,getPaymentInformationAction) {
+        'use strict';
+
+        return Component.extend({
+            defaults: {
+                redirectAfterPlaceOrder: false,
+                template: 'Intcomex_Credomatic/payment/stripe-form',
+            	/*paymentPayload: {
+                    nonce: null
+                },
+		creditCardInstallments: '',*/
+	    },
+	    
+            getCode: function() {
+                return 'Credomatic';
+            },
+
+            isActive: function() {
+                return true;
+            },
+
+            validate: function() {
+                var $form = $('#' + this.getCode() + '-form');
+                return $form.validation() && $form.validation('isValid');
+            },
+
+            afterPlaceOrder: function () { 
+
+
+                var serviceUrl = url.build('credomatic/custom/getorder');  
+                var cuotas = $("#Credomatic_installments option:selected").val();
+                var year = $("#Credomatic_expiration_yr option:selected").val();
+                var month = $("#Credomatic_expiration option:selected").val();
+                $.post(serviceUrl,{cart_id:quote.getQuoteId(),cuotas:cuotas,year:year,month:month})
+                .done(function(msg){
+                   var data = JSON.parse(msg);
+                    $("#credomatic_key_id").val(data.key_id);
+                    $("#credomatic_hash").val(data.hash);
+                    $("#credomatic_time").val(data.time);
+                    $("#credomatic_amount").val(data.amount);
+                    $("#credomatic_orderid").val(data.orderid);
+                    $("#credomatic_processor_id").val(data.processor_id);
+                    $("#credomatic_ccnumber").val($("#Credomatic_cc_number").val());
+                    $("#credomatic_ccexp").val(data.ccexp);
+                    $("#credomatic_cvv").val($("#Credomatic_cc_cid").val());
+                    $("#credomatic_redirect").val(url.build('credomatic/custom/paymentresponse'));
+                    $("#formCredomatic").attr('action',data.gateway);
+                    $("#formCredomatic").submit();
+                
+                })
+                .fail(function(msg){
+
+                })             
+
+                return false;
+            }
+	                
+            
+        });
+    }
+);
