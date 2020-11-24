168f7de81a49fd258dcdb23dc1b3ff6acf4b1185
Rafael Ferrer <rafael.ferrer@intcomex.com>
2020-11-24 05:20:33 -0500

    Ajuste credomatic respuesta JSON dvlp-REQ-{#15418}

--- app/code/Intcomex/Credomatic/Controller/Custom/GetOrder.php
+++ app/code/Intcomex/Credomatic/Controller/Custom/GetOrder.php
@@ -5,15 +5,21 @@ namespace Intcomex\Credomatic\Controller\Custom;
 class GetOrder extends \Magento\Framework\App\Action\Action
 {
 
+    /**
+    * @var \Magento\Framework\Controller\Result\JsonFactory
+    */
+    protected $resultJsonFactory;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Checkout\Model\Session $checkoutSession
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
     ) {
         parent::__construct($context);
         $this->_scopeConfig = $scopeConfig;
         $this->_checkoutSession = $checkoutSession;
+        $this->resultJsonFactory = $resultJsonFactory;
     }
 
     /**
@@ -22,13 +28,15 @@ class GetOrder extends \Magento\Framework\App\Action\Action
      * @return \Magento\Framework\Controller\ResultInterface
      */
     public function execute(){ 
-
+        
+        /** @var \Magento\Framework\Controller\Result\Json $resultJson */
+        $resultJson = $this->resultJsonFactory->create();
+        $arrayData = array();
         try {
 
             $post = $_POST; 
             $objectManager =  \Magento\Framework\App\ObjectManager::getInstance(); 
             $order = $objectManager->get('Magento\Sales\Model\Order')->getCollection()->addAttributeToSelect('*')->addFieldToFilter('entity_id',array('eq'=>$this->_checkoutSession->getLastOrderId()));
-            $arrayData = array();
             $time = strtotime(date('Y-m-d H:i:s'));
             
             $procesor_id = $this->_scopeConfig->getValue('payment/credomatic/processor_id'.$post['cuotas']);
@@ -43,14 +51,15 @@ class GetOrder extends \Magento\Framework\App\Action\Action
             $arrayData['orderid'] = $order->getData()[0]['entity_id'];
             $arrayData['gateway'] = $this->_scopeConfig->getValue('payment/credomatic/url_gateway');
             $arrayData['ccexp'] = str_pad($post['month'], 2, '0', STR_PAD_LEFT).substr($post['year'], 2, 4);
-            echo json_encode($arrayData);
-            $arrayData=array();
-
+            $arrayData['success'] = 'true';
 
         } catch (\Exception $e) {
-            $error = __('Payment create data error Credomatic: '); 
-            throw new \Magento\Framework\Validator\Exception(__($error.$e->getMessage())); 
+             
+            $arrayData = ['error' => 'true', 'message' => $e->getMessage()];
         }
+
+        $resultJson->setData($arrayData);
+        return $resultJson;
 
     }
 
