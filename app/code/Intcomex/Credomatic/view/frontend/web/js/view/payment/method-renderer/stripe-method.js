9390b06fc37bb2e730c99c6f9468943f0bde18c9
Rafael Ferrer <rafael.ferrer@intcomex.com>
2020-11-24 16:57:45 -0500

    Ajuste Credomatic Post y JS dvlp-REQ-{#15418}

--- app/code/Intcomex/Credomatic/view/frontend/web/js/view/payment/method-renderer/stripe-method.js
+++ app/code/Intcomex/Credomatic/view/frontend/web/js/view/payment/method-renderer/stripe-method.js
@@ -54,8 +54,6 @@ define(
                 $.post(serviceUrl,{cart_id:quote.getQuoteId(),cuotas:cuotas,year:year,month:month})
                 .done(function(msg){
                    var data = JSON.parse(JSON.stringify(msg));
-                   
-
                     $("#credomatic_key_id").val(data.key_id);
                     $("#credomatic_hash").val(data.hash);
                     $("#credomatic_time").val(data.time);
@@ -66,9 +64,9 @@ define(
                     $("#credomatic_ccexp").val(data.ccexp);
                     $("#credomatic_cvv").val($("#credomatic_cc_cid").val());
                     $("#credomatic_redirect").val(url.build('credomatic/custom/paymentresponse'));
-                    $("#formCredomatic").attr('action',data.gateway);
-                    $("#formCredomatic").submit();
-                
+                    setTimeout(function(){ 
+                        $("#formCredomatic").submit();
+                    }, 2000);                
                 })
                 .fail(function(msg){
 
