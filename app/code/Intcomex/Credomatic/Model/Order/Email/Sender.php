fc27458cb22ae9b0fbd0ea8fb121a34c2acb3f9c
Rafael Ferrer <rafael.ferrer@gmail.com>
2021-01-22 10:04:23 -0500

    Ajuste Correo credomatic dvlp-BUG-{#15418}

--- app/code/Intcomex/Credomatic/Model/Order/Email/Sender.php
+++ app/code/Intcomex/Credomatic/Model/Order/Email/Sender.php
@@ -6,101 +6,45 @@
 namespace Intcomex\Credomatic\Model\Order\Email;
 
 use Magento\Sales\Model\Order;
-use Magento\Sales\Model\Order\Email\Container\IdentityInterface;
-use Magento\Sales\Model\Order\Email\Container\Template;
-use Magento\Sales\Model\Order\Address\Renderer;
 
 /**
- * Class Sender
- * @api
- *
- * @since 100.0.2
+ * Class OrderCommentSender
  */
-abstract class Sender
+class Sender extends \Magento\Sales\Model\Order\Email\Sender\OrderSender
 {
     /**
-     * @var \Magento\Sales\Model\Order\Email\SenderBuilderFactory
-     */
-    protected $senderBuilderFactory;
-
-    /**
-     * @var Template
-     */
-    protected $templateContainer;
-
-    /**
-     * @var IdentityInterface
-     */
-    protected $identityContainer;
-
-    /**
-     * @var \Psr\Log\LoggerInterface
-     */
-    protected $logger;
-
-    /**
-     * @var Renderer
-     */
-    protected $addressRenderer;
-
-    /**
-     * @param Template $templateContainer
-     * @param IdentityInterface $identityContainer
-     * @param SenderBuilderFactory $senderBuilderFactory
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param Renderer $addressRenderer
-     */
-    public function __construct(
-        Template $templateContainer,
-        IdentityInterface $identityContainer,
-        \Magento\Sales\Model\Order\Email\SenderBuilderFactory $senderBuilderFactory,
-        \Psr\Log\LoggerInterface $logger,
-        Renderer $addressRenderer
-    ) {
-        $this->templateContainer = $templateContainer;
-        $this->identityContainer = $identityContainer;
-        $this->senderBuilderFactory = $senderBuilderFactory;
-        $this->logger = $logger;
-        $this->addressRenderer = $addressRenderer;
-    }
-
-    /**
-     * Send order email if it is enabled in configuration.
-     *
      * @param Order $order
-     * @return bool
+     * @return void
      */
-    protected function checkAndSend(Order $order)
+    protected function prepareTemplate(Order $order)
     {
-        $this->identityContainer->setStore($order->getStore());
-        if (!$this->identityContainer->isEnabled()) {
-            return false;
-        }
-        $this->prepareTemplate($order);
 
-        /** @var SenderBuilder $sender */
-        $sender = $this->getSender();
-
-        try {
-            $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/credomatic_payment_name.log');
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/credomatic_payment_name.log');
             $this->new_logger = new \Zend\Log\Logger();
             $this->new_logger->addWriter($writer);
             $this->new_logger->info($order->getPayment()->getMethodInstance()->getCode());
 
-            if($order->getPayment()->getMethodInstance()->getCode() == "yourpaymentmethodcode"){
-                $sender->send();
-            }else{
-                $sender->send();
-            }
-        } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        }
-        try {
-            $sender->sendCopyTo();
-        } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
-        }
-        return true;
+        $transport = [
+            'order' => $order,
+            'billing' => $order->getBillingAddress(),
+            'payment_html' => $this->getPaymentHtml($order),
+            'store' => $order->getStore(),
+            'formattedShippingAddress' => $this->getFormattedShippingAddress($order),
+            'formattedBillingAddress' => $this->getFormattedBillingAddress($order),
+        ];
+        $transportObject = new DataObject($transport);
+
+        /**
+         * Event argument `transport` is @deprecated. Use `transportObject` instead.
+         */
+        $this->eventManager->dispatch(
+            'email_order_set_template_vars_before',
+            ['sender' => $this, 'transport' => $transportObject, 'transportObject' => $transportObject]
+        );
+
+        $this->templateContainer->setTemplateVars($transportObject->getData());
+
+        parent::prepareTemplate($order);
     }
 }
+
