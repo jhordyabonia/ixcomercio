040cf018bd2426e22edd06fe5b77e97e4efa91c2
Rafael Ferrer <rafael.ferrer@gmail.com>
2021-01-21 22:08:22 -0500

    Ajustes texto informativo checkout dvlp-REQ-{#17682}

--- app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/view/billing-address.js
+++ app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/view/billing-address.js
@@ -132,7 +132,7 @@ function (
                       setTimeout(function () {
                           if(jQuery("#checkout-shipping-method-load").length>0&&window.customAlert!=''){
                               if(jQuery(".custom_alert").length==0){
-                                  jQuery("#checkout-shipping-method-load").after('<div class="custom_alert" style="color:red" ><img class="icon"  src="'+url.build('pub/media/iconos_alerta')+'/icono_'+window.currentWebsiteCode+'.png" >'+window.customAlert+'</div>');
+                                  jQuery("#checkout-shipping-method-load").after('<div class="custom_alert" style="color:red" ><img class="icon"  src="'+window.mediaUrl+'/icono_'+window.currentWebsiteCode+'.png" >'+window.customAlert+'</div>');
                               }
                               return false;
                           }
