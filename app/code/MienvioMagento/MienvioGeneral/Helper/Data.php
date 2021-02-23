cfa9b3a0ad608f8651c23e8ee89c483db2024be0
Rafael Ferrer <rafael.ferrer@intcomex.com>
2021-02-23 07:37:53 -0500

    Actualización Mienvio mienvioMagento2.10.1

--- app/code/MienvioMagento/MienvioGeneral/Helper/Data.php
+++ app/code/MienvioMagento/MienvioGeneral/Helper/Data.php
@@ -69,7 +69,7 @@ class Data extends AbstractHelper
         $result = '';
         switch ($env){
             case 0://Production
-                $result = 'https://app.mienvio.mx/';
+                $result = 'https://production.mienvio.mx/';
                 break;
             case 1: //Sandbox
                 $result = 'https://sandbox.mienvio.mx/';
