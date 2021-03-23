89c2b63945ec14327e778951f2da97dade68ac08
sergio.daza <xcbsdaza@intcomex.com>
2021-03-23 12:52:34 -0500

    Avance requerimiento #19813

---
+++ app/code/Intcomex/MercadopagoRewrites/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'MercadoPago_Core/template/payment/custom_method.html':
+                'Intcomex_MercadopagoRewrites/template/payment/custom_method.html'
+        }
+    }
+};
