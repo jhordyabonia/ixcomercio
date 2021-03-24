82562dc62c0de462096db10ba7e59bd53abc90b9
sergio.daza <xcbsdaza@intcomex.com>
2021-03-24 17:58:05 -0500

    Avance requerimiento #19813

---
+++ app/code/Intcomex/MercadopagoRewrites/Controller/Onepage/Failure.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Intcomex\MercadopagoRewrites\Controller\Onepage;
+
+class Failure extends \Magento\Checkout\Controller\Onepage
+{
+    /**
+     * @return \Magento\Framework\View\Result\Page|\Magento\Framework\Controller\Result\Redirect
+     */
+    public function execute()
+    {        
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $_checkoutSession = $objectManager->create('\Magento\Checkout\Model\Session');
+        $_quoteFactory = $objectManager->create('\Magento\Quote\Model\QuoteFactory');
+        
+        $order = $_checkoutSession->getLastRealOrder();
+        $quote = $_quoteFactory->create()->loadByIdWithoutStore($order->getQuoteId());
+        
+        if ($quote->getId()) {
+            $quote->setIsActive(1)->setReservedOrderId(null)->save();
+            $_checkoutSession->replaceQuote($quote);
+            //return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+        }
+        return $this->resultPageFactory->create();
+    }
+}
