e76595efde057f4a32ddf0bfff9120a7ecf5a696
Rafael Ferrer <rafael.ferrer@intcomex.com>
2021-04-07 00:55:59 -0500

    Ajustes Issues TradeIn dvlp-REQ-{#20758}

--- app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/model/shipping-rate-service.js
+++ app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/model/shipping-rate-service.js
@@ -23,6 +23,25 @@ define([
         } else {
             processors.default.getRates(quote.shippingAddress());
         }
+        setTimeout(function(){ 
+            
+            var serviceUrl = url.build('intcomex/custom/tradein'); 
+            jQuery.post(serviceUrl)
+            .done(function(msg){
+                if(msg.status=='success'){
+                    if(jQuery(".alert_shipping")==undefined){
+                        jQuery("#checkout-shipping-method-load").after('<div class="row tradein_alert alert_shipping" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein2+'</p></div></div>');
+                    }
+                    if(jQuery(".alert_payment")==undefined){
+                        jQuery("#checkout-payment-method-load").after('<div class="row tradein_alert alert_payment" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein1+'</p></div></div>');
+                    }
+                  }
+              })
+              .fail(function(msg){
+                  
+              })
+        
+     }, 3000);
     });
 
     return {
