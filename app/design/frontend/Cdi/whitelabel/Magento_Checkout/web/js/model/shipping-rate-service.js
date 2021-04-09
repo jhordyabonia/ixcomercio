9599e8144350f5f5e0c81bf649b78d7da0c37d5d
Rafael Ferrer <rafael.ferrer@intcomex.com>
2021-04-08 21:54:38 -0500

    AJustes agregar check de terminos y condiciones dvlp-REQ-{#20758}

--- app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/model/shipping-rate-service.js
+++ app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/model/shipping-rate-service.js
@@ -32,10 +32,10 @@
             .done(function(msg){
                 if(msg.status=='success'){
                     if(jQuery(".alert_shipping").length==0){
-                       jQuery("#checkout-shipping-method-load").after('<div class="row tradein_alert alert_shipping" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein2+'</p></div></div>');
+                       jQuery("#checkout-shipping-method-load").after('<div class="row tradein_alert alert_payment" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein2+'</p></div></div>');
                     }
                     if(jQuery(".alert_payment").length==0){
-                        jQuery("#checkout-payment-method-load").after('<div class="row tradein_alert alert_payment" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein1+'</p></div></div>');
+                        jQuery("#checkout-payment-method-load").after('<div class="row tradein_alert alert_shipping" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein1+'</p></div></div>');
                     }
                     if(msg.terms){
                         console.log('print ckeckbox');
