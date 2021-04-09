00b974eb7917d1e5fdbba3d96abf7db32a4ef8a0
Rafael Ferrer <rafael.ferrer@intcomex.com>
2021-04-08 23:32:38 -0500

    AJustes agregar check de terminos y condiciones dvlp-REQ-{#20758}

--- app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/model/shipping-rate-service.js
+++ app/design/frontend/Cdi/whitelabel/Magento_Checkout/web/js/model/shipping-rate-service.js
@@ -32,9 +32,11 @@
             .done(function(msg){
                 if(msg.status=='success'){
                     setTimeout(function(){
+                        jQuery(".alert_shipping").remove();
                         if(jQuery(".alert_shipping").length==0){
                            jQuery("#checkout-shipping-method-load").after('<div class="row tradein_alert alert_payment" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein2+'</p></div></div>');
                         }
+                        jQuery(".alert_payment").remove();
                         if(jQuery(".alert_payment").length==0){
                             jQuery("#checkout-payment-method-load").after('<div class="row tradein_alert alert_shipping" style="color:red"><div class="col-sm-1" ><img class="icon" src="'+window.mediaUrl+'/iconos_alerta/icono_'+window.currentWebsiteCode+'.png"></div><div class="col-sm-11" ><p>'+window.alertaTradein1+'</p></div></div>');
                         }
