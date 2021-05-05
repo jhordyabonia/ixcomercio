3d24792ee5ee0fbc0fb62a2cec32533086532032
xcbeport@intcomex.com <edu.porti@hotmail.com>
2021-05-05 10:43:04 -0500

    Test xml REQ #22191

--- app/code/Intcomex/Xtento/Controller/Index/Index.php
+++ app/code/Intcomex/Xtento/Controller/Index/Index.php
@@ -22,7 +22,8 @@ class Index extends \Magento\Framework\App\Action\Action
 	public function execute()
 	{
 		$token = $this->getRequest()->getParam('token');
-		print_r($token);
+		echo "Se imprime token ";
+		echo $token;
 		$post = $this->_xtxmlFactory->create();
 		$collection = $post->getCollection();
         header('Content-type: text/xml');
