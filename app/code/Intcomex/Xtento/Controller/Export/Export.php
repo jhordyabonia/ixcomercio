a346a3737affc418b0d6d9d7feac0fb24b167e0f
xcbeport@intcomex.com <edu.porti@hotmail.com>
2021-05-05 17:25:23 -0500

    Test xml REQ #22191

--- app/code/Intcomex/Xtento/Controller/Export/Export.php
+++ app/code/Intcomex/Xtento/Controller/Export/Export.php
@@ -8,15 +8,18 @@ class Export extends \Magento\Framework\App\Action\Action
 	protected $request;
 	protected $_xtxmlFactory;
 	protected $collection;
+	protected $resourceConnection;
 
 	public function __construct(
 		\Magento\Framework\App\Action\Context $context,
 		\Magento\Framework\View\Result\PageFactory $pageFactory,
 		\Intcomex\Xtento\Model\XtxmlFactory $xtxmlFactory,
 		\Intcomex\Xtento\Model\ResourceModel\Xtxml\CollectionFactory $CollectionFactory,
-		\Magento\Framework\App\Request\Http $request
+		\Magento\Framework\App\Request\Http $request,
+		\Magento\Framework\App\ResourceConnection $resourceConnection
 		)
 	{
+		$this->resourceConnection = $resourceConnection;
 		$this->_pageFactory = $pageFactory;
 		$this->_xtxmlFactory = $xtxmlFactory;
 		$this->request = $request;
@@ -24,9 +27,38 @@ class Export extends \Magento\Framework\App\Action\Action
 		return parent::__construct($context);
 	}
 
+	/**
+	* Get Table name using direct query
+	*/
+	public function getTablename($tableName)
+	{
+
+        /* Create Connection */
+
+        $connection  = $this->resourceConnection->getConnection();
+
+        $tableName   = $connection->getTableName($tableName);
+
+        return $tableName;
+
+    }
+
 	public function execute()
 	{
 		$token = $this->request->getParam('token');
+
+		$tableName = $this->getTableName('xtento_catalog_xml');
+		$query = "SELECT * FROM " . 
+							$tableName .
+							" WHERE token = ".
+							$token;
+		/**
+		* Execute the query and store the results in $results variable
+		*/
+		$results = $this->resourceConnection->getConnection()->fetchAll($query);
+		echo "<pre>";print_r($results);
+		die();
+
 		$post = $this->_xtxmlFactory->create();
 		$collection = $post->getCollection();
         header('Content-type: text/xml');
