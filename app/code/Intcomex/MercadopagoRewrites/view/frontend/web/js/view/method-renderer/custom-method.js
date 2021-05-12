03aae7203a0c4785e314b8b8cf182c33c0e8fc48
Rafael Ferrer <xcbrferr@intcomex.com>
2021-05-12 14:32:16 -0500

    Ajuetes desplegables meses y años dvlp-REQ-{#23224}

--- app/code/Intcomex/MercadopagoRewrites/view/frontend/web/js/view/method-renderer/custom-method.js
+++ app/code/Intcomex/MercadopagoRewrites/view/frontend/web/js/view/method-renderer/custom-method.js
@@ -23,19 +23,19 @@ define([
                 var code_payment = $(input).attr('id');
 
 
-                $(menu).prepend('<li role="presentation" class="payment-group-item debitcard active"><a id="link-'+ code_payment+ '" data-code="'+ code_payment+ '">'+title+'</a></li>');
+                $(menu).prepend('<li role="presentation" class="payment-group-item debitcard active"><a id="link-' + code_payment + '" data-code="' + code_payment + '">' + title + '</a></li>');
 
-                $('#'+code_payment).trigger( "click" );
+                $('#' + code_payment).trigger("click");
 
-                $(document).on('click', `#payment_methods_menu ul li a#link-`+code_payment, function (event) {
+                $(document).on('click', `#payment_methods_menu ul li a#link-` + code_payment, function (event) {
 
                     var data = $(this).attr('data-code');
 
-                    $('#'+data).trigger( "click" );
+                    $('#' + data).trigger("click");
 
-                    if($(this).parent().hasClass('active')){
+                    if ($(this).parent().hasClass('active')) {
 
-                    }else{
+                    } else {
                         $(menu).find('li.active').removeClass('active');
                         $(this).parent().addClass('active');
                     }
@@ -43,7 +43,62 @@ define([
                 });
 
 
-            }
+            },
+            /**
+         * Get list of available month values
+         * @returns {Object}
+         */
+            getCcMonthsValues: function () {
+                console.log('test Rewrite getCcMonthsValues');
+                return _.map(this.getCcMonths(), function (value, key) {
+                    return {
+                        'value': key,
+                        'month': value
+                    };
+                });
+            },
+            /**
+         * Get list of months
+         * @returns {Object}
+         */
+            getCcMonths: function () {
+                var ccMonths = window.checkoutConfig.payment.ccform.months[this.getCode()];
+                var newCcMonths = new Object;
+                for (const i in ccMonths) {
+                    if (i <= 12) {
+                        newCcMonths[i] = ccMonths[i];
+                    }
+                }
+                return newCcMonths;
+            },
+            /**
+         * Get list of available year values
+         * @returns {Object}
+         */
+            getCcYearsValues: function () {
+                return _.map(this.getCcYears(), function (value, key) {
+                    return {
+                        'value': key,
+                        'year': value
+                    };
+                });
+            },
+            /**
+             * Get list of years
+             * @returns {Object}
+             */
+            getCcYears: function () {
+                var ccYears = window.checkoutConfig.payment.ccform.years[this.getCode()];
+                var newCcYears = new Object;
+                var count = 1;
+                for (var j in ccYears) {
+                    if (count <= 11) {
+                        newCcYears[j] = ccYears[j];
+                    }
+                    count++;
+                }
+                return newCcYears;
+            },
         });
     }
 });
