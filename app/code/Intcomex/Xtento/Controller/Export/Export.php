3456310fac400fc2bfc84592bde03f03e39e698f
xcbeport@intcomex.com <edu.porti@hotmail.com>
2021-05-12 15:21:05 -0500

    Ajuste de cron REQ #22191

--- app/code/Intcomex/Xtento/Controller/Export/Export.php
+++ app/code/Intcomex/Xtento/Controller/Export/Export.php
@@ -47,9 +47,8 @@ class Export extends \Magento\Framework\App\Action\Action
 	{
 		$token = $this->request->getParam('token');
         $post = $this->_xtxmlFactory->create();
-				$collection = $post->getCollection()->addFieldToFilter('token', array('eq' => $token))->addAttributeToSort('fecha', 'desc')->setPageSize(1)
-				->setCurPage(1)->getData();
-        header('Content-type: text/xml');
+				$collection = $post->getCollection()->addFieldToFilter('token', array('eq' => $token))->setOrder('fecha','DESC')->setPageSize(1)->getData();
+				header('Content-type: text/xml');
         print($collection[0]['xml']);
         exit();
 		return $this->_pageFactory->create();
