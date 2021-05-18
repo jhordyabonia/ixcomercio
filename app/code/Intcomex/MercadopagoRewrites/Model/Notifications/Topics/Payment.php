f621ddc98a58cbb1b2924f48f69308685c1185ca
Johan Martinez <johan.martinez@intcomex.com>
2021-05-18 17:37:40 -0500

    Ajuste ap MP

--- app/code/Intcomex/MercadopagoRewrites/Model/Notifications/Topics/Payment.php
+++ app/code/Intcomex/MercadopagoRewrites/Model/Notifications/Topics/Payment.php
@@ -21,7 +21,7 @@ use MercadoPago\Core\Helper\Response;
 use MercadoPago\Core\Lib\RestClient;
 use MercadoPago\Core\Model\Core;
 
-class Payment 
+class Payment extends \MercadoPago\Core\Model\Notifications\Topics\TopicsAbstract
 {
     const LOG_NAME    = 'notification_payment';
     const TYPES_TOPIC = [
@@ -75,7 +75,6 @@ class Payment
     }//end __construct()
 
 
-
     /**
      * @param  $payment
      * @return array
@@ -84,11 +83,12 @@ class Payment
     public function updateStatusOrderByPayment($payment)
     {
         //$order = parent::getOrderByIncrementId($payment['external_reference']);
+
         $objectManager=\Magento\Framework\App\ObjectManager::getInstance();
 
         $helper = $objectManager->get('Intcomex\MercadopagoRewrites\Helper\Api');
 
-        $order = $helper->getOrdenByIncrementId('17000000159');
+        $order = $helper->getOrdenByIncrementId($payment['external_reference']);
 
         if (!$order->getId()) {
             $message = 'Mercado Pago - The order was not found in Magento. You will not be able to follow the process without this information.';
@@ -156,4 +156,175 @@ class Payment
 
     }//end updateStatusOrderByPayment()
 
-}
+
+    /**
+     * @param  $paymentResponse
+     * @param  $order
+     * @return boolean
+     */
+    public function checkStatusAlreadyUpdated($paymentResponse, $order)
+    {
+        $orderUpdated   = false;
+        $statusToUpdate = parent::getConfigStatus($paymentResponse, false);
+        $commentsObject = $order->getStatusHistoryCollection(true);
+        foreach ($commentsObject as $commentObj) {
+            if ($commentObj->getStatus() == $statusToUpdate) {
+                $orderUpdated = true;
+            }
+        }
+
+        return $orderUpdated;
+
+    }//end checkStatusAlreadyUpdated()
+
+
+    /**
+     * @param $order
+     * @param $message
+     */
+    public function sendEmailCreateOrUpdate($order, $message)
+    {
+        $emailOrderCreate = $this->_scopeConfig->getValue(ConfigData::PATH_ADVANCED_EMAIL_CREATE, ScopeInterface::SCOPE_STORE);
+        $emailAlreadySent = false;
+        if ($emailOrderCreate) {
+            if (!$order->getEmailSent()) {
+                $this->_orderSender->send($order, true);
+                $emailAlreadySent = true;
+            }
+        }
+
+        if ($emailAlreadySent === false) {
+            $statusEmail     = $this->_scopeConfig->getValue(ConfigData::PATH_ADVANCED_EMAIL_UPDATE, ScopeInterface::SCOPE_STORE);
+            $statusEmailList = explode(',', $statusEmail);
+            if (in_array($order->getStatus(), $statusEmailList)) {
+                $this->_orderCommentSender->send($order, $notify = '1', str_replace('<br/>', '', $message));
+            }
+        }
+
+    }//end sendEmailCreateOrUpdate()
+
+
+    /**
+     * @param  $order
+     * @param  $message
+     * @return boolean
+     * @throws Exception
+     */
+    public function createInvoice($order, $message)
+    {
+        if (!$order->hasInvoices()) {
+            $invoice = $order->prepareInvoice();
+            $invoice->register();
+            $invoice->pay();
+            $invoice->addComment(str_replace('<br/>', '', $message), false, true);
+
+            $transaction = $this->_transactionFactory->create();
+            $transaction->addObject($invoice);
+            $transaction->addObject($invoice->getOrder());
+            $transaction->save();
+            $this->_invoiceSender->send($invoice, true, $message);
+            return true;
+        }
+
+        return false;
+
+    }//end createInvoice()
+
+
+    /**
+     * @param  $paymentResponse
+     * @return array
+     */
+    public function addCardInCustomer($paymentResponse)
+    {
+        if (isset($paymentResponse['metadata'])
+            && isset($paymentResponse['metadata']['customer_id'])
+            && isset($paymentResponse['metadata']['token'])
+            && isset($paymentResponse['payment_method_id'])
+            && isset($paymentResponse['issuer_id'])
+        ) {
+            $customer_id       = $paymentResponse['metadata']['customer_id'];
+            $token             = $paymentResponse['metadata']['token'];
+            $payment_method_id = $paymentResponse['payment_method_id'];
+            $issuer_id         = (int) $paymentResponse['issuer_id'];
+
+            $accessToken = $this->_scopeConfig->getValue(ConfigData::PATH_ACCESS_TOKEN, ScopeInterface::SCOPE_STORE);
+            $request     = [
+                'token'             => $token,
+                'issuer_id'         => $issuer_id,
+                'payment_method_id' => $payment_method_id,
+            ];
+            $card        = RestClient::post('/v1/customers/'.$customer_id.'/cards', $request, null, ['Authorization: Bearer '.$accessToken]);
+            return $card;
+        }
+
+    }//end addCardInCustomer()
+
+
+    /**
+     * @param  $id
+     * @param  null $type
+     * @return array
+     */
+    public function getPaymentData($id, $type=null)
+    {
+        try {
+            $response = $this->_coreModel->getPayment($id);
+            $this->_mpHelper->log('Response API MP Get Payment', self::LOG_NAME, $response);
+
+            if (!$this->isValidResponse($response)) {
+                throw new Exception(__('MP API Invalid Response'), 400);
+            }
+
+            $payments   = [];
+            $payments[] = $response['response'];
+
+            return [
+                'merchantOrder' => null,
+                'payments'      => $payments,
+                'shipmentData'  => null,
+            ];
+        } catch (\Exception $e) {
+            $this->_mpHelper->log(__('ERROR - Notifications Payment getPaymentData'), self::LOG_NAME, $e->getMessage());
+        }
+
+    }//end getPaymentData()
+
+
+    /**
+     * @param Order $order
+     * @param array $payment
+     */
+    protected function updateAdditionalInformation($order, $payment)
+    {
+        $orderPayment          = $order->getPayment();
+        $additionalInformation = $orderPayment->getAdditionalInformation('paymentResponse');
+
+        if (!empty($payment['card'])) {
+            $card = $payment['card'];
+
+            if (isset($card['last_four_digits'])) {
+                $orderPayment->setCcLast4($card['last_four_digits']);
+            }
+
+            if (isset($card['expiration_month'])) {
+                $orderPayment->setCcExpMonth($card['expiration_month']);
+            }
+
+            if (isset($card['expiration_year'])) {
+                $orderPayment->setCcExpYear($card['expiration_year']);
+            }
+
+            $orderPayment->setCcType($payment['payment_method_id']);
+            $orderPayment->setCcTransId($payment['id']);
+        }
+
+        $additionalInformation['status']        = $payment['status'];
+        $additionalInformation['status_detail'] = $payment['status_detail'];
+
+        $order->getPayment()->setAdditionalInformation('paymentResponse', $additionalInformation);
+
+    }//end updateAdditionalInformation()
+
+
+}//end class
