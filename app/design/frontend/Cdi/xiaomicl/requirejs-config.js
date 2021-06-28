f26c5eb2c3e5ad9d6a77c5421c3a605ab81f2a97
Rafael Ferrer <xcbrferr@intcomex.com>
2021-06-28 07:03:32 -0500

    Ajustes Formulario xiaomicl dvlp-REQ-{#25303}

--- app/design/frontend/Cdi/xiaomicl/requirejs-config.js
+++ app/design/frontend/Cdi/xiaomicl/requirejs-config.js
@@ -3,7 +3,9 @@ var config = {
       '*': {
         bootstrap: "js/bootstrap.bundle",
         owlCarouselJs: "js/owl.carousel.min",
-        mainJs: "js/main.min"
+        mainJs: "js/main.min",
+        'MercadoPago_Core/js/view/method-renderer/custom_method':
+        'Magento_Checkout/js/view/method-renderer/mercado-pago/custom_method',
 		}
 	},
     paths: {
