b6d023f85217e75a084093421777e04e6c46712c
Rafael Ferrer <xcbrferr@intcomex.com>
2021-06-28 09:36:23 -0500

    Ajustes Xiaomi Chile dvlp-REQ-{#25303}

--- app/design/frontend/Cdi/xiaomicl/requirejs-config.js
+++ app/design/frontend/Cdi/xiaomicl/requirejs-config.js
@@ -3,9 +3,7 @@ var config = {
       '*': {
         bootstrap: "js/bootstrap.bundle",
         owlCarouselJs: "js/owl.carousel.min",
-        mainJs: "js/main.min",
-        'MercadoPago_Core/js/view/method-renderer/custom_method':
-        'Magento_Checkout/js/view/method-renderer/mercado-pago/custom_method',
+        mainJs: "js/main.min"
 		}
 	},
     paths: {
