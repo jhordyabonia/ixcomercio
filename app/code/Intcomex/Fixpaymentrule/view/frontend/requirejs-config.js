da9ee012c39f911b0cd20c2f313cddcd36a0ae39
Rafael Ferrer <xcbrferr@intcomex.com>
2021-07-22 10:40:57 -0500

    Ajustes Reglas de carrito metodo de pago dvlp-REQ-{#27240}

---
+++ app/code/Intcomex/Fixpaymentrule/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+        map: {
+            '*': {
+                'Magento_Checkout/js/action/select-payment-method':
+                    'Intcomex_Fixpaymentrule/js/action/select-payment-method'
+            }
+        }
+    };
