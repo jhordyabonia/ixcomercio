f206586888397f7a2c75d5217ae10533b9427175
Rafael Ferrer <xcbrferr@intcomex.com>
2021-08-02 14:13:18 -0500

    Ajustes TimeOut Credomatic Visa y MasterCard dvlp-REQ-{#27630}

--- app/code/Intcomex/Credomatic/view/frontend/web/js/view/payment/method-renderer/stripe-method.js
+++ app/code/Intcomex/Credomatic/view/frontend/web/js/view/payment/method-renderer/stripe-method.js
@@ -82,12 +82,13 @@ define(
                              if(i==1){
                                 var redirectUrl = urlPaymentResponse+'?orderid='+data['orderid']+'&empty=true';
                                 window.location.href = redirectUrl;
+                                return false;
                              }
                             if (--i) {          // If i > 0, keep going
                             theLoop(i);       // Call the loop again, and pass it the current value of i
                             }
                         }, 9000);
-                    })(5); 
+                    })(6); 
                     
                 })
                 .fail(function(msg){
