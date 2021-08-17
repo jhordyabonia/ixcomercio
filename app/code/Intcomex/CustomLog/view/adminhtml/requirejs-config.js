e0e3ea5f6d895bbef370711ddd40dda2046a4237
Rafael Ferrer <xcbrferr@intcomex.com>
2021-08-17 17:32:44 -0500

    Ajuste Use Default Value dvlp-REQ-{#27471}

--- app/code/Intcomex/CustomLog/view/adminhtml/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        '*': {
-          'Magento_Catalog/view/adminhtml/web/template/form/field.html': 
-            'Intcomex_CustomLog/view/adminhtml/web/template/form/field.html'
-        }
-  }
-};
