6e2a3a32a5baea8694dca1a0b5469bde0b0c48ea
Rafael Ferrer <xcbrferr@intcomex.com>
2021-08-17 13:18:13 -0500

    Ajustes Validacion de precio dvlp-REQ-{#27471}

---
+++ app/code/Intcomex/CustomLog/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+          'Magento_Catalog/view/adminhtml/web/template/form/field.html': 
+            'Intcomex_CustomLog/view/adminhtml/web/template/form/field.html'
+        }
+  }
+};
