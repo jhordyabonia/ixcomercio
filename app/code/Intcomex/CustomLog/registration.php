38177e0bdf58b2c16b524692975a03a4b40e9471
German Dario Cardenas Patiño <xcbgcard@intcomex.com>
2021-08-18 09:05:27 -0500

    commit para probar que todo este OK con el repositorio

--- app/code/Intcomex/CustomLog/registration.php
+++ app/code/Intcomex/CustomLog/registration.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © Intcomex, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
