b87f355b6d78ea16690c28fc7f860b206c2a90a0
Johan Martinez <johan.martinez@intcomex.com>
2021-09-22 10:26:39 -0500

    Ajuste de module

--- app/code/Intcomex/FormatPrice/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Intcomex_FormatPrice',
-    __DIR__
-);
