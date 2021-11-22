f288211c51539b51b38fec95c1de4750d27d5989
Johan Martinez <johan.martinez@intcomex.com>
2021-11-22 10:31:38 -0500

    Add theme Nokia CO REQ 31311

--- app/design/frontend/Cdi/nokiaco/registration.php
+++ app/design/frontend/Cdi/nokiaco/registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Cdi/zaggco',
+    'frontend/Cdi/nokiaco',
     __DIR__
 );
