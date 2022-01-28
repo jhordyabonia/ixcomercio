97d837e940d71e534dfa17be0ae8fd868ad1b9c7
Jhon Salazar <jsala326@intcomex.com>
2022-01-27 22:59:41 -0500

    Se agregan variables de pago y envio en correo de transaccion en validacion

--- app/code/Intcomex/Clearsale/Helper/Email.php
+++ app/code/Intcomex/Clearsale/Helper/Email.php
@@ -10,7 +10,9 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\MailException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Payment\Helper\Data as PaymentHelper;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Address\Renderer;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -58,10 +60,22 @@ class Email extends AbstractHelper
     protected $_logger;
 
     /**
+     * @var PaymentHelper
+     */
+    protected $_paymentHelper;
+
+    /**
+     * @var Renderer
+     */
+    protected $_addressRenderer;
+
+    /**
      * @param ScopeConfigInterface $scopeConfig
      * @param StoreManagerInterface $storeManager
      * @param TransportBuilder $transportBuilder
      * @param LoggerInterface $logger
+     * @param PaymentHelper $paymentHelper
+     * @param Renderer $addressRenderer
      * @param Context $context
      */
     public function __construct(
@@ -69,12 +83,16 @@ class Email extends AbstractHelper
         StoreManagerInterface $storeManager,
         TransportBuilder $transportBuilder,
         LoggerInterface $logger,
+        PaymentHelper $paymentHelper,
+        Renderer $addressRenderer,
         Context $context
     ) {
         $this->_scopeConfig = $scopeConfig;
         $this->_storeManager = $storeManager;
         $this->_transportBuilder = $transportBuilder;
         $this->_logger = $logger;
+        $this->_paymentHelper = $paymentHelper;
+        $this->_addressRenderer = $addressRenderer;
         parent::__construct($context);
     }
 
@@ -93,9 +111,9 @@ class Email extends AbstractHelper
                 'store' => $order->getStore(),
                 'subject' => $this->scopeConfig->getValue(self::PATH_TRANSACTION_IN_VALIDATION_SUBJECT, ScopeInterface::SCOPE_STORE, $order->getStoreId()),
                 'text' => $this->scopeConfig->getValue(self::PATH_TRANSACTION_IN_VALIDATION_TEXT, ScopeInterface::SCOPE_STORE, $order->getStoreId()),
-//            'payment_html' => $this->getPaymentHtml($order),
-//            'formattedShippingAddress' => $this->getFormattedShippingAddress($order),
-//            'formattedBillingAddress' => $this->getFormattedBillingAddress($order),
+                'payment_html' => $this->getPaymentHtml($order),
+                'formattedShippingAddress' => $this->getFormattedShippingAddress($order),
+                'formattedBillingAddress' => $this->getFormattedBillingAddress($order),
             ];
             $transportObject = new DataObject($transport);
             $transport = $this->_transportBuilder
@@ -115,5 +133,48 @@ class Email extends AbstractHelper
                 $this->_logger->error($e->getMessage());
             }
         }
+    }
+
+    /**
+     * Get payment info block as html.
+     *
+     * @param Order $order
+     * @return string
+     * @throws NoSuchEntityException
+     */
+    protected function getPaymentHtml(Order $order): string
+    {
+        try {
+            return $this->_paymentHelper->getInfoBlockHtml(
+                $order->getPayment(),
+                $this->_storeManager->getStore()->getId()
+            );
+        } catch (\Exception $e) {
+            $this->_logger->error($e->getMessage());
+        }
+    }
+
+    /**
+     * Render shipping address into html.
+     *
+     * @param Order $order
+     * @return string|null
+     */
+    protected function getFormattedShippingAddress(Order $order): ?string
+    {
+        return $order->getIsVirtual()
+            ? null
+            : $this->_addressRenderer->format($order->getShippingAddress(), 'html');
+    }
+
+    /**
+     * Render billing address into html.
+     *
+     * @param Order $order
+     * @return string|null
+     */
+    protected function getFormattedBillingAddress(Order $order): ?string
+    {
+        return $this->_addressRenderer->format($order->getBillingAddress(), 'html');
     }
 }
--- app/code/Intcomex/Clearsale/Observer/EmailTransactionValidation.php
+++ app/code/Intcomex/Clearsale/Observer/EmailTransactionValidation.php
@@ -5,6 +5,9 @@ namespace Intcomex\Clearsale\Observer;
 use Intcomex\Clearsale\Helper\Email;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\MailException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Sales\Model\Order;
 
 class EmailTransactionValidation implements ObserverInterface
@@ -25,11 +28,14 @@ class EmailTransactionValidation implements ObserverInterface
 
     /**
      * @param Observer $observer
+     * @throws LocalizedException
+     * @throws MailException
+     * @throws NoSuchEntityException
      */
     public function execute(Observer $observer)
     {
         /** @var Order $order */
-        $order = $observer->getOrder();
+        $order = $observer->getData('order');
         if ($order && $order->getPayment()->getMethod() === 'adyen_cc' && $order->getStatus() !== 'approved_clearsale') {
             $this->_helperEmail->sendTransactionInValidationMail($order);
         }
