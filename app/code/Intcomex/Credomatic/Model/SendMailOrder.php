48529c30c7632a14ad61fde7465d7f2a2083fbe7
Rafael Ferrer <rafael.ferrer@vasslatam.com>
2022-02-25 18:02:04 -0500

    Ajustes ispaid master-BUG-{#ControlOrderNotification}

--- app/code/Intcomex/Credomatic/Model/SendMailOrder.php
+++ app/code/Intcomex/Credomatic/Model/SendMailOrder.php
@@ -53,7 +53,7 @@ class SendMailOrder extends \Magento\Sales\Model\Order\Email\Sender\OrderSender 
             }else{
                 //$paymentResp = json_decode($payment->getAdditionalInformation('payment_resp'),true);
                 $orderData = $order->getData();
-                $this->logger->info(print_r($orderData,true));
+                //$this->logger->info(print_r($orderData,true));
 
                 $isPaid = (isset($orderData['is_paid']))?$orderData['is_paid']:0;
                 $this->logger->info('getIsPaid '.$isPaid);
