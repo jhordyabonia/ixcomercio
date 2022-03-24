d1ddd029fc3d2eb40779bb4541e8b42394ea5798
Jhon Salazar <jsala326@intcomex.com>
2022-03-24 18:43:41 -0500

    Req #23534. Se agrega modulo base trabajado en el requerimiento #17656

---
+++ app/code/Intcomex/Auditoria/Controller/Adminhtml/Import/Validate.php
@@ -0,0 +1,272 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Intcomex\Auditoria\Controller\Adminhtml\Import;
+
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\ImportExport\Controller\Adminhtml\ImportResult as ImportResultController;
+use Magento\ImportExport\Model\Import;
+use Magento\ImportExport\Block\Adminhtml\Import\Frame\Result;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\ImportExport\Model\Import\Adapter as ImportAdapter;
+
+/**
+ * Import validate controller action.
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Validate extends ImportResultController implements HttpPostActionInterface
+{
+    /**
+     * @var Import
+     */
+    private $import;
+
+    /**
+     * Validate uploaded files action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
+    public function execute()
+    {
+        $data = $this->getRequest()->getPostValue();
+        /** @var \Magento\Framework\View\Result\Layout $resultLayout */
+        $resultLayout = $this->resultFactory->create(ResultFactory::TYPE_LAYOUT);
+        /** @var $resultBlock Result */
+        $resultBlock = $resultLayout->getLayout()->getBlock('import.frame.result');
+        if ($data) {
+            // common actions
+            $resultBlock->addAction(
+                'show',
+                'import_validation_container'
+            );
+
+            /** @var $import \Magento\ImportExport\Model\Import */
+            $import = $this->getImport()->setData($data);
+            try {
+                $source = $import->uploadFileAndGetSource();
+                $erroPrice = array();
+                if($data['entity']=='catalog_product'||$data['entity']=='advanced_pricing'){
+                   $erroPrice =  $this->validatePrice($data,$source);
+                }
+                $this->processValidationResult($import->validateSource($source), $resultBlock,$erroPrice);
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $resultBlock->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $resultBlock->addError(__('Sorry, but the data is invalid or the file is not uploaded.'));
+            }
+            return $resultLayout;
+        } elseif ($this->getRequest()->isPost() && empty($_FILES)) {
+            $resultBlock->addError(__('The file was not uploaded.'));
+            return $resultLayout;
+        }
+        $this->messageManager->addError(__('Sorry, but the data is invalid or the file is not uploaded.'));
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect->setPath('adminhtml/*/index');
+        return $resultRedirect;
+    }
+
+    /**
+     * Process validation result and add required error or success messages to Result block
+     *
+     * @param bool $validationResult
+     * @param Result $resultBlock
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function processValidationResult($validationResult, $resultBlock,$erroPrice)
+    {
+        $import = $this->getImport();
+        $errorAggregator = $import->getErrorAggregator();
+        if(!empty($erroPrice)){
+            $resultBlock->addError(
+                __('Productos con valores fuera de parametros '.implode(',',$erroPrice))
+            );
+            $this->addErrorMessages($resultBlock, $errorAggregator);
+        }else{
+            if ($import->getProcessedRowsCount()) {
+                if ($validationResult) {
+                    $this->addMessageForValidResult($resultBlock);
+                } else {
+                    $resultBlock->addError(
+                        __('Data validation failed. Please fix the following errors and upload the file again.')
+                    );
+    
+                    if ($errorAggregator->getErrorsCount()) {
+                        $this->addMessageToSkipErrors($resultBlock);
+                    }
+                }
+                $resultBlock->addNotice(
+                    __(
+                        'Checked rows: %1, checked entities: %2, invalid rows: %3, total errors: %4',
+                        $import->getProcessedRowsCount(),
+                        $import->getProcessedEntitiesCount(),
+                        $errorAggregator->getInvalidRowsCount(),
+                        $errorAggregator->getErrorsCount()
+                    )
+                );
+                
+                $this->addErrorMessages($resultBlock, $errorAggregator);
+            } else {
+                if ($errorAggregator->getErrorsCount()) {
+                    $this->collectErrors($resultBlock);
+                } else {
+                    $resultBlock->addError(__('This file is empty. Please try another one.'));
+                }
+            }
+        }
+    }
+
+    /**
+     * Provides import model.
+     *
+     * @return Import
+     * @deprecated 100.1.0
+     */
+    private function getImport()
+    {
+        if (!$this->import) {
+            $this->import = $this->_objectManager->get(Import::class);
+        }
+        return $this->import;
+    }
+
+    /**
+     * Add error message to Result block and allow 'Import' button
+     *
+     * If validation strategy is equal to 'validation-skip-errors' and validation error limit is not exceeded,
+     * then add error message and allow 'Import' button.
+     *
+     * @param Result $resultBlock
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function addMessageToSkipErrors(Result $resultBlock)
+    {
+        $import = $this->getImport();
+        if (!$import->getErrorAggregator()->hasFatalExceptions()) {
+            $resultBlock->addSuccess(
+                __('Please fix errors and re-upload file or simply press "Import" button to skip rows with errors'),
+                true
+            );
+        }
+    }
+
+    /**
+     * Add success message to Result block
+     *
+     * 1. Add message for case when imported data was checked and result is valid.
+     * 2. Add message for case when imported data was checked and result is valid, but import is not allowed.
+     *
+     * @param Result $resultBlock
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function addMessageForValidResult(Result $resultBlock)
+    {
+        if ($this->getImport()->isImportAllowed()) {
+            $resultBlock->addSuccess(__('File is valid! To start import process press "Import" button'), true);
+        } else {
+            $resultBlock->addError(__('The file is valid, but we can\'t import it for some reason.'));
+        }
+    }
+
+    /**
+     * Collect errors and add error messages to Result block
+     *
+     * Get all errors from Error Aggregator and add appropriated error messages
+     * to Result block.
+     *
+     * @param Result $resultBlock
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function collectErrors(Result $resultBlock)
+    {
+        $import = $this->getImport();
+        $errors = $import->getErrorAggregator()->getAllErrors();
+        foreach ($errors as $error) {
+            $resultBlock->addError($error->getErrorMessage());
+        }
+    }
+
+    public function validatePrice($data,$source){
+
+        //\Intcomex\Auditoria\Helper\Email
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); 
+        $helper = $objectManager->get('\Intcomex\Auditoria\Helper\Email');
+        $productFactory = $objectManager->get('\Magento\Catalog\Model\ProductFactory');
+        $scopeConfig = $objectManager->get('\Magento\Framework\App\Config\ScopeConfigInterface');
+        $storesRepository = $objectManager->get('\Magento\Store\Api\StoreRepositoryInterface');
+
+        $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/validacion_Product_Import.log');
+        $this->logger = new \Zend\Log\Logger();
+        $this->logger->addWriter($writer);
+        $this->logger->info('Validación '.$data['entity']);
+        
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $porcentaje = $scopeConfig->getValue('auditoria/general/porcentaje_validacion', $storeScope);
+    
+        $products = $productFactory->create();
+
+        $style = 'style="border:1px solid"';
+
+        $path = $this->getProtectedValue($this->getProtectedValue($source,'_file'),'path');
+        $errorsSku = array();
+        $errors = '';
+        if (($handle = fopen($path, "r")) !== FALSE) {
+            $csvFile = file($path);
+            foreach ($csvFile as $key => $line) {
+                if($key>0){
+                    $dataLine = explode($data['_import_field_separator'],str_getcsv($line)[0]);
+                    $storeId = $storesRepository->get(trim($dataLine[1]))->getId();
+                    $products->setStoreId($storeId);
+                    $sku = $dataLine[0];
+                    $productObj = $products->loadByAttribute('sku',trim($sku));
+
+                    $price = $dataLine[2];
+                        if($dataLine[2]==''||empty($dataLine[2])||$dataLine[2]>$dataLine[3]){
+                            $price = $dataLine[3];
+                        }
+                    $precioReferencia = $productObj->getPrecioReferencia();
+                    $validar =  ($precioReferencia*(int)$porcentaje)/100;
+                    $this->logger->info('Se evalua '.$sku.' para '.$dataLine[1]);
+                    $this->logger->info('Precio a actualizar :'.$price);
+                    $this->logger->info('Precio de referencia: '.$precioReferencia.' * '.$porcentaje.' / 100 = '.$validar);
+                    $this->logger->info(' ------- ');
+                    if($precioReferencia!=''&&!empty($precioReferencia)){
+                        if($price<$validar){
+                            $errors .= '<tr>';
+                            $errors .= '<td '.$style.' >'.$sku.'</td>';
+                            $errors .= '<td '.$style.' >'.$dataLine[1].'</td>';
+                            $errors .= '<td '.$style.' >'.$price.'</td>';
+                            $errors .= '<td '.$style.' >Precio</td>';
+                            $errors .= '</tr>';
+                            $errorsSku[] = $sku;
+                        }
+                    }else{
+                        $this->logger->info(' No se puede evaluar  '.$sku.' No tiene precio referencia en: '.$dataLine[1]);
+                    }
+                }
+            }
+            if($errors!=''){
+                $extraError = 'Estas ingresando valores no permitidos en el producto';
+                $helper->notify('Soporte Whitelabel',$errors,$extraError, 0);
+            }
+        }
+        
+        return $errorsSku;
+
+    }
+
+
+    function getProtectedValue($obj, $name) {
+        $array = (array)$obj;
+        $prefix = chr(0).'*'.chr(0);
+        return $array[$prefix.$name];
+    }
+}
---
+++ app/code/Intcomex/Auditoria/Helper/Email.php
@@ -0,0 +1,196 @@
+<?php
+namespace Intcomex\Auditoria\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Email extends AbstractHelper
+{
+    protected $scopeConfig;
+
+    const XML_PATH_EMAIL_TEMPLATE_FIELD = 'trax_catalogo/catalogo_general/template_notification';
+
+    /**
+     * Sender email config path - from default CONTACT extension
+     */
+    const XML_PATH_EMAIL_SENDER = 'contact/email/sender_email_identity';
+
+    /**
+     * Store manager
+     *
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var \Magento\Framework\Translate\Inline\StateInterface
+     */
+    private $inlineTranslation;
+
+    /**
+     * @var \Magento\Framework\Mail\Template\TransportBuilder
+     */
+    private $transportBuilder;
+
+    /**
+     * Email constructor.
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation
+     * @param \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
+        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Store\Api\StoreRepositoryInterface $storesRepository
+    ) {
+        parent::__construct($context);
+        $this->storeManager = $storeManager;
+        $this->inlineTranslation = $inlineTranslation;
+        $this->transportBuilder = $transportBuilder;
+        $this->scopeConfig = $scopeConfig;
+        $this->_storesRepository = $storesRepository;
+    }
+
+    /**
+     * Return store configuration value of your template field that which id you set for template
+     *
+     * @param string $path
+     * @param int $storeid
+     * @return mixed
+     */
+    private function getConfigValue($path, $storeid)
+    {
+        return $this->scopeConfig->getValue(
+            $path,
+            ScopeInterface::SCOPE_STORE,
+            $storeid
+        );
+    }
+
+    /**
+     * Return store
+     * @return \Magento\Store\Api\Data\StoreInterface
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getStore()
+    {
+        return $this->storeManager->getStore();
+    }
+
+    /**
+     * @param $variable
+     * @param $receiverInfo
+     * @param $templateId
+     * @return $this
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function generateTemplate($variable, $receiverInfo, $senderInfo, $templateId, $storeid)
+    {
+        $this->transportBuilder->setTemplateIdentifier($templateId)
+            ->setTemplateOptions(
+                [
+                    'area' => \Magento\Framework\App\Area::AREA_ADMINHTML,
+                    'store' => $storeid,
+                ]
+            )
+            ->setTemplateVars($variable)
+            ->setFrom($senderInfo)
+            ->addTo($receiverInfo['email'], $receiverInfo['name']);
+
+        return $this;
+    }
+
+    /**
+     * Return email for sender header
+     * @return mixed
+     */
+    public function emailSender()
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_EMAIL_SENDER,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * @param $name
+     * @param $email
+     * @return $this
+     * @throws \Magento\Framework\Exception\MailException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function notify($name,$data,$extraError, $storeid)
+    {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $mensaje_alerta = $this->scopeConfig->getValue('auditoria/general/mensaje_alerta', $storeScope);
+        $email = explode(',',$this->scopeConfig->getValue('auditoria/general/correos_alerta', $storeScope));
+
+       
+        
+        /* Sender Detail  */
+        $senderInfo = [
+            'name' => 'Whitelabel Store',
+            'email' => 'soporteb2c@ixcomercio.com',
+        ];
+
+        /* Assign values for your template variables  */
+        $variable = [];
+        $variable['mensaje_alerta'] = $mensaje_alerta;
+        $variable['data'] = $data;
+        $variable['extra_error'] = $extraError;
+
+        $templateId = "email_auditoria_template";
+        foreach($email as $key => $value){
+            if(!empty($value)){
+                 /* Receiver Detail */
+                $receiverInfo = [
+                    'name' => $name,
+                    'email' => $value
+                ];
+
+                $this->inlineTranslation->suspend();
+                $this->generateTemplate($variable, $receiverInfo, $senderInfo, $templateId, $storeid);
+                $transport = $this->transportBuilder->getTransport();
+                $transport->sendMessage();
+                $this->inlineTranslation->resume();
+            }
+        }
+
+        return $this;
+    }
+
+    public function clearSpecialCharac($String){
+        $String = str_replace(array('á','à','â','ã','ª','ä'),"a",$String);
+        $String = str_replace(array('Á','À','Â','Ã','Ä'),"A",$String);
+        $String = str_replace(array('Í','Ì','Î','Ï'),"I",$String);
+        $String = str_replace(array('í','ì','î','ï'),"i",$String);
+        $String = str_replace(array('é','è','ê','ë'),"e",$String);
+        $String = str_replace(array('É','È','Ê','Ë'),"E",$String);
+        $String = str_replace(array('ó','ò','ô','õ','ö','º'),"o",$String);
+        $String = str_replace(array('Ó','Ò','Ô','Õ','Ö'),"O",$String);
+        $String = str_replace(array('ú','ù','û','ü'),"u",$String);
+        $String = str_replace(array('Ú','Ù','Û','Ü'),"U",$String);
+        $String = str_replace(array('^','´','`','¨','~'),"",$String);
+        $String = str_replace("ç","c",$String);
+        $String = str_replace("Ç","C",$String);
+        $String = str_replace("ñ","n",$String);
+        $String = str_replace("Ñ","n",$String);
+        $String = str_replace("Ý","Y",$String);
+        $String = str_replace("ý","y",$String);
+        $String = str_replace("&aacute;","a",$String);
+        $String = str_replace("&Aacute;","A",$String);
+        $String = str_replace("&eacute;","e",$String);
+        $String = str_replace("&Eacute;","E",$String);
+        $String = str_replace("&iacute;","i",$String);
+        $String = str_replace("&Iacute;","I",$String);
+        $String = str_replace("&oacute;","o",$String);
+        $String = str_replace("&Oacute;","O",$String);
+        $String = str_replace("&uacute;","u",$String);
+        $String = str_replace("&Uacute;","U",$String);
+        return $String;
+    }
+}
