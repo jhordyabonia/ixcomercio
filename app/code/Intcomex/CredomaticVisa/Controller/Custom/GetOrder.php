f9ec7a324d80d6cdc9d408f818b7acaa773d60c7
Freddy Ahumada <yfahumcw1@intcomex.com>
2022-04-22 20:27:28 +0000

    fix getOrder controller credomaticMastercard dvlp-REQ-{#45196-V1}

--- app/code/Intcomex/CredomaticVisa/Controller/Custom/GetOrder.php
+++ app/code/Intcomex/CredomaticVisa/Controller/Custom/GetOrder.php
@@ -49,25 +49,32 @@ class GetOrder extends \Magento\Framework\App\Action\Action
             $orderId =  $this->_checkoutSession->getLastOrderId();
             $order = $this->_modelOrder->load($orderId);
 
-            $billingAddress = $order->getBillingAddress();
-            $arrayData['firstname'] = $billingAddress->getFirstname();
-            $arrayData['lastname'] = $billingAddress->getLastname();
-            $arrayData['email'] = $billingAddress->getEmail();
-            $arrayData['phone'] = $billingAddress->getTelephone();
-            $arrayData['address1'] = isset($billingAddress->getStreet()[0]) ? $billingAddress->getStreet()[0] : '';
-            $arrayData['address2'] = isset($billingAddress->getStreet()[1]) ? $billingAddress->getStreet()[1] : '';
-
             $arrayData['key'] = $this->_scopeConfig->getValue('payment/credomaticvisa/key',ScopeInterface::SCOPE_STORE);
             $arrayData['key_id'] = $this->_scopeConfig->getValue('payment/credomaticvisa/key_id',ScopeInterface::SCOPE_STORE);
             $arrayData['processor_id'] = $this->_scopeConfig->getValue('payment/credomaticvisa/processor_id'.$post['cuotas'],ScopeInterface::SCOPE_STORE);
             $arrayData['amount'] = number_format($order->getGrandTotal(),2,".","");
             $arrayData['orderid'] = $order->getIncrementId();
             $token = substr(md5(uniqid(rand())), 0, 49);
-            $arrayData['url_resp'] = $this->storeManagerInterface->getStore()->getBaseUrl().'credomaticvisa/custom/registerresponse?token='.$token.'';
-            $arrayData['url_gateway'] = $this->_scopeConfig->getValue('payment/credomaticvisa/url_gateway',ScopeInterface::SCOPE_STORE);
 
+            $arrayData['type'] = 'sale';
+            $arrayData['key'] = $this->_scopeConfig->getValue('payment/credomaticvisa/key',ScopeInterface::SCOPE_STORE);
+            $arrayData['key_id'] = $this->_scopeConfig->getValue('payment/credomaticvisa/key_id',ScopeInterface::SCOPE_STORE);
+            $arrayData['amount'] = number_format($order->getGrandTotal(),2,".","");
             $arrayData['time'] = strtotime(date('Y-m-d H:i:s'));
             $arrayData['hash'] = md5($order->getIncrementId().'|'.$arrayData['amount'].'|'.$arrayData['time'].'|'.$this->_scopeConfig->getValue('payment/credomatic/key',ScopeInterface::SCOPE_STORE));
+            $arrayData['orderid'] = $order->getIncrementId();
+            $arrayData['processor_id'] = $this->_scopeConfig->getValue('payment/credomaticvisa/processor_id'.$post['cuotas'],ScopeInterface::SCOPE_STORE);
+            $arrayData['firstname'] = $billingAddress->getFirstname();
+            $arrayData['lastname'] = $billingAddress->getLastname();
+            $arrayData['email'] = $billingAddress->getEmail();
+            $arrayData['phone'] = $billingAddress->getTelephone();
+            $arrayData['street1'] = isset($billingAddress->getStreet()[0]) ? $billingAddress->getStreet()[0] : '';
+            $arrayData['street2'] = isset($billingAddress->getStreet()[1]) ? $billingAddress->getStreet()[1] : '';
+            $arrayData['ccexp'] = str_pad($post['month'], 2, '0', STR_PAD_LEFT).substr($post['year'], 2, 4);
+            $arrayData['redirect'] = $this->storeManagerInterface->getStore()->getBaseUrl().'credomaticvisa/custom/paymentresponse?token='.$token.'';
+            $arrayData['url_gateway'] = $this->_scopeConfig->getValue('payment/credomaticvisa/url_gateway',ScopeInterface::SCOPE_STORE);
+            $url_gateway = $this->_scopeConfig->getValue('payment/credomaticvisa/url_gateway',ScopeInterface::SCOPE_STORE);
+
             
             $model =  $this->_credomaticFactory->create();
             $model->addData([
