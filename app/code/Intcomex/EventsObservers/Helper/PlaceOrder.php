1bdac86ff072eee9b248a946fdd750af910b6e9f
Daniel Duran <jdura375@intcomex.com>
2022-06-08 10:52:50 -0500

    factura electronica - dvlp-REQ-{#45613}

--- app/code/Intcomex/EventsObservers/Helper/PlaceOrder.php
+++ app/code/Intcomex/EventsObservers/Helper/PlaceOrder.php
@@ -321,6 +321,7 @@ class PlaceOrder extends AbstractHelper
         }else {
             $identifi = $this->getIdentification($billing,$shipping);
         }
+        error_log("IDENTIFI XXXXXXXXXXXXX:".$identifi);
 
         $discount = abs($order->getGiftCardsAmount()) + abs($order->getBaseDiscountAmount());
         $payload = array(
