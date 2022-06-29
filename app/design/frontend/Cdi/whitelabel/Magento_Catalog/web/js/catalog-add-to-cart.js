e5d3e8cc62f068f0a1d4a01850173cb1a4405ccd
Cron server <cron@ixcomercio.com>
2022-06-29 19:57:14 +0000

    comparador de productos dvlp-REQ-{#29096}

--- app/design/frontend/Cdi/whitelabel/Magento_Catalog/web/js/catalog-add-to-cart.js
+++ app/design/frontend/Cdi/whitelabel/Magento_Catalog/web/js/catalog-add-to-cart.js
@@ -12,36 +12,36 @@ define([
 ], function ($, $t, _, idsResolver) {
     'use strict';
     $('body').on('click', '.button-carousel', function() {
-        console.log($(this).data("item"))
-        switch ($(this).data("item")) {
-            case 1:
-                $('.carousel1').removeClass("carousel-hidden").addClass('carousel-visible');
-                $('.carousel2').removeClass("carousel-visible").addClass('carousel-hidden');
-                $('.carousel3').removeClass("carousel-visible").addClass('carousel-hidden');
-                $('.carousel4').removeClass("carousel-visible").addClass('carousel-hidden');
-                break;
-            case 2:
-                $('.carousel1').removeClass("carousel-visible").addClass('carousel-hidden');
-                $('.carousel2').removeClass("carousel-hidden").addClass('carousel-visible');
-                $('.carousel3').removeClass("carousel-visible").addClass('carousel-hidden');
-                $('.carousel4').removeClass("carousel-visible").addClass('carousel-hidden');
-                break;
-            case 3:
-                $('.carousel1').css('display', 'none');
-                $('.carousel2').css('display', 'none');
-                $('.carousel3').css('display', 'block');
-                $('.carousel4').css('display', 'none');
-                break;
-            case 4:
-                $('.carousel1').css('display', 'none');
-                $('.carousel2').css('display', 'none');
-                $('.carousel3').css('display', 'none');
-                $('.carousel4').css('display', 'block');
-                break;
-            default:
-                break;
-        }
-    });
+            console.log($(this).data("item"))
+            switch ($(this).data("item")) {
+                case 1:
+                    $('.carousel1').removeClass("carousel-hidden").addClass('carousel-visible');
+                    $('.carousel2').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel3').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel4').removeClass("carousel-visible").addClass('carousel-hidden');
+                    break;
+                case 2:
+                    $('.carousel1').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel2').removeClass("carousel-hidden").addClass('carousel-visible');
+                    $('.carousel3').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel4').removeClass("carousel-visible").addClass('carousel-hidden');
+                    break;
+                case 3:
+                    $('.carousel1').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel2').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel3').removeClass("carousel-hidden").addClass('carousel-visible');
+                    $('.carousel4').removeClass("carousel-visible").addClass('carousel-hidden');
+                    break;
+                case 4:
+                    $('.carousel1').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel2').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel3').removeClass("carousel-visible").addClass('carousel-hidden');
+                    $('.carousel4').removeClass("carousel-hidden").addClass('carousel-visible');
+                    break;
+                default:
+                    break;
+            }
+        });    
     $.widget('mage.catalogAddToCart', {
         options: {
             processStart: null,
--- app/design/frontend/Cdi/whitelabel/web/js/carousel.js
+++ app/design/frontend/Cdi/whitelabel/web/js/carousel.js
@@ -2,7 +2,6 @@ define(['jquery'], function($){
   "use strict";
       return function myscript()
       {
-          alert('oprimio el boton ');
-          //put all your myfile js code here
+          
       }
 });
