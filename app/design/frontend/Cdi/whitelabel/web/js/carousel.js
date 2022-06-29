723e5b246a29ac436da2b1c049330b1eaf3d6cb7
xcbeport@intcomex.com <edu.porti@hotmail.com>
2022-06-29 14:58:03 -0500

    Adding new classes to template V6 {#29096}

--- app/design/frontend/Cdi/whitelabel/web/js/carousel.js
+++ app/design/frontend/Cdi/whitelabel/web/js/carousel.js
@@ -3,6 +3,7 @@ define(['jquery'], function($){
       return function myscript()
       {
           alert('oprimio el boton ');
+          console.log("estcribo en la cosola de prueba");
           //put all your myfile js code here
       }
 });
