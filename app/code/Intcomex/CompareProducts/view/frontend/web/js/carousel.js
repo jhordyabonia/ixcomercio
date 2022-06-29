e26a239b35c4ecc8185fe9c15e541ae9776fc7b0
xcbeport@intcomex.com <edu.porti@hotmail.com>
2022-06-29 15:17:07 -0500

    Adding new classes to template V6 {#29096}

--- app/code/Intcomex/CompareProducts/view/frontend/web/js/carousel.js
+++ app/code/Intcomex/CompareProducts/view/frontend/web/js/carousel.js
@@ -1,9 +1,14 @@
-define(['jquery'], function($){
+define([
+    'jquery'
+], function ($) {
   "use strict";
-      return function myscript()
+
+  alert('oprimio el boton ');
+  console.log("estcribo en la cosola de prueba");
+      /*return function myscript()
       {
           alert('oprimio el boton ');
           console.log("estcribo en la cosola de prueba");
           //put all your myfile js code here
-      }
+      }*/
 });
