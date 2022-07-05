93fbefb1152e95fbc97ca0bc16ed068de397074f
Luis Castaño Rodriguez <xcblcast@intcomex.com>
2022-07-05 11:40:37 -0500

    Se crea el contenido del archivo unistall data del modulo bss ProductStockAlert #45975-v5

--- app/code/Bss/ProductStockAlert/Setup/UninstallData.php
+++ app/code/Bss/ProductStockAlert/Setup/UninstallData.php
@@ -21,8 +21,24 @@ namespace Bss\ProductStockAlert\Setup\UninstallData;
 
 class UninstallData implements \Magento\Framework\Setup\UninstallInterface
 {
-    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
-    {
+    /**
+     * @var \Magento\Eav\Setup\EavSetupFactory
+     */
+    private $eavSetupFactory;
 
+    public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory){
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function uninstall(
+        \Magento\Framework\Setup\SchemaSetupInterface $setup,
+        \Magento\Framework\Setup\ModuleContextInterface $context
+    )
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->removeAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'product_stock_alert'
+        );
     }
 }
