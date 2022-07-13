f7c7bca5a7797018d7cde349a9ded0e40ec66d3c
xcbeport@intcomex.com <edu.porti@hotmail.com>
2022-07-12 19:07:14 -0500

    Adding popup pro to app folder {#45472}

---
+++ app/code/Aheadworks/Popup/Block/Adminhtml/Menu.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Aheadworks Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://ecommerce.aheadworks.com/end-user-license-agreement/
+ *
+ * @package    Popup
+ * @version    1.2.3
+ * @copyright  Copyright (c) 2020 Aheadworks Inc. (http://www.aheadworks.com)
+ * @license    https://ecommerce.aheadworks.com/end-user-license-agreement/
+ */
+namespace Aheadworks\Popup\Block\Adminhtml;
+
+/**
+ * Class Menu
+ * @package Aheadworks\Popup\Block\Adminhtml
+ */
+class Menu extends \Magento\Backend\Block\Template
+{
+    const ITEM_BLOCK = 'block';
+    const ITEM_README = 'readme';
+    const ITEM_SUPPORT = 'support';
+    const ITEM_CONFIG = 'system_config';
+
+    /**
+     * Current item key
+     * @var string|null
+     */
+    private $currentItemKey = null;
+
+    /**
+     * Path to template file in theme.
+     *
+     * @var string
+     */
+    protected $_template = 'Aheadworks_Popup::menu.phtml';
+
+    /**
+     * Get menu items
+     *
+     * @return array
+     */
+    public function getItems()
+    {
+        return [
+            self::ITEM_BLOCK => [
+                'title' => __('Manage Popups'),
+                'url' => $this->getUrl('popup_admin/popup/index')
+            ],
+            self::ITEM_CONFIG => [
+                'title' => __('Settings'),
+                'url' => $this->getUrl('adminhtml/system_config/edit', ['section' => 'aw_popup'])
+            ],
+            self::ITEM_README => [
+                'title' => __('Readme'),
+                'url' => 'https://aheadworks.atlassian.net/wiki/spaces/EUDOC/pages/1229226551/Popup+Pro+-+Magento+2',
+                'target' => '__blank',
+                'class' => 'aw-extensions-menu-separator'
+            ],
+            self::ITEM_SUPPORT => [
+                'title' => __('Get Support'),
+                'url' => ' http://ecommerce.aheadworks.com/contacts/',
+                'target' => '__blank',
+            ],
+        ];
+    }
+
+    /**
+     * Get current menu item
+     *
+     * @return string
+     */
+    public function getCurrentItemKey()
+    {
+        return $this->currentItemKey;
+    }
+
+    /**
+     * Set current menu item
+     *
+     * @param string $key
+     *
+     * @return $this
+     */
+    public function setCurrentItemKey($key)
+    {
+        $this->currentItemKey = $key;
+        return $this;
+    }
+
+    /**
+     * Get current menu title
+     *
+     * @return string
+     */
+    public function getCurrentItemTitle()
+    {
+        $items = $this->getItems();
+        $key = $this->getCurrentItemKey();
+        if (!array_key_exists($key, $items)) {
+            return '';
+        }
+        return $items[$key]['title'];
+    }
+}
---
+++ app/code/Aheadworks/Popup/Block/Adminhtml/Popup/Edit.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Aheadworks Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://ecommerce.aheadworks.com/end-user-license-agreement/
+ *
+ * @package    Popup
+ * @version    1.2.3
+ * @copyright  Copyright (c) 2020 Aheadworks Inc. (http://www.aheadworks.com)
+ * @license    https://ecommerce.aheadworks.com/end-user-license-agreement/
+ */
+namespace Aheadworks\Popup\Block\Adminhtml\Popup;
+
+/**
+ * Class Edit
+ * @package Aheadworks\Popup\Block\Adminhtml\Popup
+ */
+class Edit extends \Magento\Backend\Block\Widget\Form\Container
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    private $coreRegistry = null;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\Block\Widget\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Widget\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        $this->coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+
+        $this->_objectId = 'id';
+        $this->_blockGroup = 'Aheadworks_Popup';
+        $this->_controller = 'adminhtml_popup';
+        $this->setId('popup_edit');
+        parent::_construct();
+
+        /* @var $model \Aheadworks\Popup\Model\Popup */
+        $model = $this->coreRegistry->registry('aw_popup_model');
+        if ($model && $model->getId()) {
+            $this->buttonList->update('save', 'class_name', \Magento\Backend\Block\Widget\Button\SplitButton::class);
+            $this->buttonList->update('save', 'options', $this->_getSaveButtonOptions());
+        }
+        $this->buttonList->add(
+            'save_and_continue',
+            [
+                'label' => __('Save and Continue Edit'),
+                'class' => 'save',
+                'data_attribute' => [
+                    'mage-init' => [
+                        'button' => ['event' => 'saveAndContinueEdit', 'target' => '#edit_form'],
+                    ],
+                ]
+            ],
+            0
+        );
+    }
+
+    /**
+     * Get options for save button
+     *
+     * @return array
+     */
+    protected function _getSaveButtonOptions()
+    {
+        return [
+            [
+                'label' => __('Save'),
+                'data_attribute' => [
+                    'mage-init' => ['button' => ['event' => 'save', 'target' => '#edit_form']],
+                ],
+                'default' => true
+            ],
+            [
+                'label' => __('Save as New'),
+                'data_attribute' => [
+                    'mage-init' => ['button' => ['event' => 'saveAndNew', 'target' => '#edit_form']],
+                ],
+                'default' => false
+            ]
+        ];
+    }
+
+    /**
+     * Get URL for back (reset) button
+     *
+     * @return string
+     */
+    public function getBackUrl()
+    {
+        return $this->getUrl('*/popup/');
+    }
+
+    /**
+     * Get URL for delete button
+     *
+     * @return string
+     */
+    public function getDeleteUrl()
+    {
+        return $this->getUrl('*/popup/delete', ['_current' => true]);
+    }
+
+    /**
+     * Prepare layout
+     *
+     * @return \Magento\Framework\View\Element\AbstractBlock
+     */
+    protected function _prepareLayout()
+    {
+        $this->_formScripts[] = '
+            require([
+                "jquery",
+                "mage/mage"
+            ], function($){
+                var $form = $("#edit_form");
+                    $form.mage("form", {
+                    handlersData: {
+                        saveAndNew: {
+                            action: {
+                                args: {back: "new"}
+                            }
+                        },
+                    }
+                });
+            });';
+        return parent::_prepareLayout();
+    }
+}
